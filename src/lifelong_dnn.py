84468596a41bd28d018dac190a2486ecd58bfe06
levinwv1 <william.levine@jhuapl.edu>
2020-04-30 11:23:54 -0400

    matched interface

--- src/lifelong_dnn.py
+++ src/lifelong_dnn.py
@@ -5,7 +5,7 @@ from sklearn.base import clone
 import numpy as np
 
 class LifeLongDNN():
-    def __init__(self):
+    def __init__(self, acorn = None):
         self.X_across_tasks = []
         self.y_across_tasks = []
         
@@ -13,15 +13,28 @@ class LifeLongDNN():
         
         #element [i, j] votes on task i under transformation from task j
         self.voters_across_tasks_matrix = []
-        self.num_tasks = 0
+        self.n_tasks = 0
         
         self.classes_across_tasks = []
         
+        if acorn is not None:
+            np.random.seed(acorn)
+        
     def check_task_idx_(self, task_idx):
-        if task_idx >= self.num_tasks:
+        if task_idx >= self.n_tasks:
             raise Exception("Invalid Task IDX")
     
-    def add_task(self, X, y, epochs = 100, lr = 5e-4):
+    def new_forest(self, 
+                   X, 
+                   y, 
+                   epochs = 100, 
+                   lr = 5e-4, 
+                   n_estimators = 100, 
+                   max_samples = .32,
+                   boostrap = True,
+                   max_depth = 30,
+                   min_samples_leaf = 1,
+                   acorn = None):
         
         self.X_across_tasks.append(X)
         self.y_across_tasks.append(y)
@@ -36,7 +49,7 @@ class LifeLongDNN():
         self.classes_across_tasks.append(new_classes)
         
         #add one voter to previous task voter lists under the new transformation
-        for task_idx in range(self.num_tasks):            
+        for task_idx in range(self.n_tasks):            
             X_of_task, y_of_task = self.X_across_tasks[task_idx], self.y_across_tasks[task_idx]
             X_of_task_under_new_transform = new_transformer.predict(X_of_task) 
             
@@ -45,9 +58,9 @@ class LifeLongDNN():
 
             self.voters_across_tasks_matrix[task_idx].append(task_voter_under_new_transformation)
             
-        #add num_tasks voters to new task voter list under previous transformations 
+        #add n_tasks voters to new task voter list under previous transformations 
         new_voters_under_previous_task_transformation = []
-        for task_idx in range(self.num_tasks):
+        for task_idx in range(self.n_tasks):
             transformer_of_task = self.transformers_across_tasks[task_idx]
             X_under_task_transformation = transformer_of_task.predict(X)
             
@@ -60,22 +73,24 @@ class LifeLongDNN():
         
         self.voters_across_tasks_matrix.append(new_voters_under_previous_task_transformation)
         
-        self.num_tasks += 1
+        self.n_tasks += 1
         
-    def predict_proba(self, X, target_task_idx, transformer_task_idxs = None):
-        self.check_task_idx_(target_task_idx)
+    def _estimate_posteriors(self, test, X, representation = 0, decider = 0):
+        self.check_task_idx_(decider)
         
-        if transformer_task_idxs == None:
-            transformer_task_idxs = range(self.num_tasks)
+        if representation == "all":
+            representation = range(self.n_tasks)
+        elif isinstance(representation, int):
+            representation = np.array([representation])
         
         posteriors_across_tasks = []
-        for transformer_task_idx in transformer_task_idxs:
+        for transformer_task_idx in representation:
             transformer = self.transformers_across_tasks[transformer_task_idx]
-            voter = self.voters_across_tasks_matrix[target_task_idx][transformer_task_idx]
+            voter = self.voters_across_tasks_matrix[decider][transformer_task_idx]
             posteriors_across_tasks.append(voter.predict_proba(transformer.predict(X)))
         return np.mean(posteriors_across_tasks, axis = 0)
     
-    def predict(self, X, target_task_idx, transformer_task_idxs = None):
-        task_classes = self.classes_across_tasks[target_task_idx]
-        return task_classes[np.argmax(self.predict_proba(X, target_task_idx, transformer_task_idxs), axis = 1)]
+    def predict(self, X, representation = 0, decider = 0):
+        task_classes = self.classes_across_tasks[decider]
+        return task_classes[np.argmax(self._estimate_posteriors(X, representation, decider), axis = 1)]
         
