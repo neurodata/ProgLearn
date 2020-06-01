d243518d5f7fae114a546c803d6e7408111af827
jdey4 <jdey4@jhmi.edu>
2020-06-01 18:49:02 -0400

    cifar exp

--- experiments/cifar_exp/plot_cifar_all_algo.py
+++ experiments/cifar_exp/plot_cifar_all_algo.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 import numpy as np
+import pandas as pd
 from itertools import product
 import seaborn as sns
 
-# %%
+#%%
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
@@ -25,7 +26,6 @@ def get_fte_bte(err, single_err, ntrees):
             te[i].append(single_err[i]/err[j][i])
                 
     for i in range(10):
-        #print(single_err[i],err[i][i])
         fte.append(single_err[i]/err[i][i])
             
             
@@ -62,5 +62,260 @@ def calc_mean_fte(ftes,task_num=10,reps=6):
     fte = np.asarray(ftes)
     
     return list(np.mean(np.asarray(fte),axis=0))
-  
+
+def get_error_matrix(filename):
+    multitask_df, single_task_df = unpickle(filename)
+
+    err = [[] for _ in range(10)]
+
+    for ii in range(10):
+        err[ii].extend(
+            1 - np.array(
+                multitask_df[multitask_df['base_task']==ii+1]['accuracy']
+                )
+            )
+    single_err = 1 - np.array(single_task_df['accuracy'])
+
+    return single_err, err
+
+#%%
+### MAIN HYPERPARAMS ###
+ntrees = 10
+slots = 10
+task_num = 10
+shifts = 6
+total_alg = 9
+alg_name = ['L2F(cap.)', 'L2F', 'L2N','Prog-NN', 'DF-CNN','EWC','O-EWC','SI','LwF']
+model_file = ['uf10','fixed_uf10','dnn0','Prog_NN','DF_CNN','EWC', 'Online_EWC', 'SI', 'LwF']
+btes = [[] for i in range(total_alg)]
+ftes = [[] for i in range(total_alg)]
+tes = [[] for i in range(total_alg)]
+########################
+
+#%% code for L2F and L2N
+reps = slots*shifts
+
+for alg in range(3): 
+    count = 0 
+    te_tmp = [[] for _ in range(reps)]
+    bte_tmp = [[] for _ in range(reps)]
+    fte_tmp = [[] for _ in range(reps)] 
+
+    for slot in range(slots):
+        for shift in range(shifts):
+            filename = 'result/'+model_file[alg]+'_'+str(shift+1)+'_'+str(slot)+'.pickle'
+            multitask_df, single_task_df = unpickle(filename)
+
+            single_err, err = get_error_matrix(filename)
+            fte, bte, te = get_fte_bte(err,single_err,ntrees)
+            
+            te_tmp[count].extend(te)
+            bte_tmp[count].extend(bte)
+            fte_tmp[count].extend(fte)
+            count+=1
+    
+    tes[alg].extend(calc_mean_te(te_tmp,reps=reps))
+    btes[alg].extend(calc_mean_bte(bte_tmp,reps=reps))
+    ftes[alg].extend(calc_mean_fte(fte_tmp,reps=reps))
+
+#%%
+reps = shifts
+
+for alg in range(3,total_alg): 
+    count = 0  
+    te_tmp = [[] for _ in range(reps)]
+    bte_tmp = [[] for _ in range(reps)]
+    fte_tmp = [[] for _ in range(reps)]
+
+    for shift in range(shifts):
+        filename = 'benchmarking_algorthms_result/'+model_file[alg]+'_'+str(shift+1)+'.pickle'
+        multitask_df, single_task_df = unpickle(filename)
+
+        single_err, err = get_error_matrix(filename)
+        fte, bte, te = get_fte_bte(err,single_err,ntrees)
+         
+        te_tmp[count].extend(te)
+        bte_tmp[count].extend(bte)
+        fte_tmp[count].extend(fte)
+        count+=1
+    
+    tes[alg].extend(calc_mean_te(te_tmp,reps=reps))
+    btes[alg].extend(calc_mean_bte(bte_tmp,reps=reps))
+    ftes[alg].extend(calc_mean_fte(fte_tmp,reps=reps))
+#%%
+te = {'L2Fc':np.zeros(10,dtype=float), 'L2F':np.zeros(10,dtype=float), 'L2N':np.zeros(10,dtype=float), 'Prog-NN':np.zeros(10,dtype=float), 'DF-CNN':np.zeros(10,dtype=float),'EWC':np.zeros(10,dtype=float), 'Online EWC':np.zeros(10,dtype=float), 'SI':np.zeros(10,dtype=float), 'LwF':np.zeros(10,dtype=float)}
+
+for count,name in enumerate(te.keys()):
+    for i in range(10):
+        te[name][i] = tes[count][i][9-i]
+
+df = pd.DataFrame.from_dict(te)
+df = pd.melt(df,var_name='Algorithms', value_name='Transfer Efficieny')
+
+mean_te = {'L2Fc':[np.mean(te['L2Fc'])], 'L2F':[np.mean(te['L2F'])], 
+           'L2N':[np.mean(te['L2N'])], 'Prog-NN':[np.mean(te['Prog-NN'])], 
+           'DF-CNN':[np.mean(te['DF-CNN'])],'EWC':[np.mean(te['EWC'])], 
+           'Online EWC':[np.mean(te['Online EWC'])], 'SI':[np.mean(te['SI'])], 
+           'LwF':[np.mean(te['LwF'])]}
+mean_df = pd.DataFrame.from_dict(mean_te)
+mean_df = pd.melt(mean_df,var_name='Algorithms', value_name='Transfer Efficieny')
+
+#%%
+clr = ["#e41a1c", "#e41a1c", "#00008B", "#a65628", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#CCCC00"]
+c = sns.color_palette(clr, n_colors=len(clr))
+
+fontsize=24
+ticksize=20
+
+fig, ax = plt.subplots(2,2, figsize=(16,11.5))
+# plt.subplots_adjust(right=0.5)
+for i, fte in enumerate(ftes):
+    if i == 0:
+        ax[0][0].plot(np.arange(1,11), fte, c=clr[i], marker='.', markersize=14, label=alg_name[i], linewidth=3)
+        continue
+        
+    if i == 1:
+        ax[0][0].plot(np.arange(1,11), fte, c=clr[i], marker='.', linestyle='dashed', markersize=12, label=alg_name[i])
+        continue
+    
+    ax[0][0].plot(np.arange(1,11), fte, c=clr[i], marker='.', markersize=12, label=alg_name[i])
+    
+ax[0][0].set_xticks(np.arange(1,11))
+ax[0][0].set_yticks([0.9, 1.1, 1.3])
+ax[0][0].set_ylim(0.85, 1.41)
+ax[0][0].tick_params(labelsize=ticksize)
+# ax[0].legend(algos, loc='upper left', fontsize=14)
+# ax[0].legend(algos, bbox_to_anchor=(1.2, -.2), loc=2, borderaxespad=0)
+
+ax[0][0].set_ylabel('Forward Transfer Efficiency', fontsize=fontsize)
+ax[0][0].set_xlabel('Number of tasks seen', fontsize=fontsize)
+
+ax[0][0].grid(axis='x')
+
+for i in range(task_num - 1):
+
+    et = np.zeros((total_alg,task_num-i))
+
+    for j in range(0,total_alg):
+        et[j,:] = np.asarray(btes[j][i])
+
+    ns = np.arange(i + 1, task_num + 1)
+    for j in range(0,total_alg):
+        if j == 0:
+            if i == 0:
+                ax[0][1].plot(ns, et[j,:], label = alg_name[j], c=c[j], linewidth = 2.6)
+            else:
+                ax[0][1].plot(ns, et[j,:], c=c[j], linewidth = 2.6)
+        elif j==1:
+            if i == 0:
+                ax[0][1].plot(ns, et[j,:], label = alg_name[j], c=c[j], linestyle='dashed')
+            else:
+                ax[0][1].plot(ns, et[j,:], c=c[j], linestyle='dashed')
+        else:
+            if i == 0:
+                ax[0][1].plot(ns, et[j,:], label = alg_name[j], c=c[j])
+            else:
+                ax[0][1].plot(ns, et[j,:], c=c[j])
+
+
+# ax[1].set_title(ttle, fontsize=20)
+ax[0][1].set_xlabel('Number of tasks seen', fontsize=fontsize)
+ax[0][1].set_ylabel('Backward Transfer Efficiency', fontsize=fontsize)
+# ax.set_ylim(0.05 - 0.01, 0.5 + 0.01)
+# box = ax.get_position()
+# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# ax[1].legend(loc='upper left', fontsize=12)
+ax[0][1].legend(loc='center left', bbox_to_anchor=(1,0.5), fontsize=22)
+ax[0][1].set_yticks([.4,.6,.8,.9,1, 1.1,1.2])
+ax[0][1].set_xticks(np.arange(1,11))
+ax[0][1].set_ylim(0.85, 1.19)
+ax[0][1].tick_params(labelsize=ticksize)
+ax[0][1].grid(axis='x')
+
+
+for i in range(task_num- 1):
+
+    et = np.zeros((total_alg,task_num-i))
+
+    for j in range(0,total_alg):
+        et[j,:] = np.asarray(tes[j][i])
+
+    ns = np.arange(i + 1, task_num + 1)
+    for j in range(0,total_alg):
+        if j == 0:
+            if i == 0 or i==1:
+                ax[1][0].plot(ns, et[j,:], label = alg_name[j], c=c[j], linewidth = 2.6)
+            else:
+                ax[1][0].plot(ns, et[j,:], c=c[j], linewidth = 2.6)
+                
+        elif j == 1:
+            if i == 0 or i==1:
+                ax[1][0].plot(ns, et[j,:], label = alg_name[j], c=c[j], linestyle='dashed')
+            else:
+                ax[1][0].plot(ns, et[j,:], c=c[j], linestyle='dashed')
+        else:
+            if i == 0 or i==1:
+                ax[1][0].plot(ns, et[j,:], label = alg_name[j], c=c[j])
+            else:
+                ax[1][0].plot(ns, et[j,:], c=c[j])
+
+
+# ax[1].set_title(ttle, fontsize=20)
+ax[1][0].set_xlabel('Number of tasks seen', fontsize=fontsize)
+ax[1][0].set_ylabel('Transfer Efficiency', fontsize=fontsize)
+# ax.set_ylim(0.05 - 0.01, 0.5 + 0.01)
+# box = ax.get_position()
+# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# ax[1].legend(loc='upper left', fontsize=12)
+#ax[1][0].legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=22)
+ax[1][0].set_yticks([.4,.6,.8,.9,1, 1.1,1.2])
+ax[1][0].set_xticks(np.arange(1,11))
+ax[1][0].set_ylim(0.85, 1.26)
+ax[1][0].tick_params(labelsize=ticksize)
+ax[1][0].grid(axis='x')
+
+ax[0][0].hlines(1, 1,10, colors='grey', linestyles='dashed',linewidth=1.5)
+ax[0][1].hlines(1, 1,10, colors='grey', linestyles='dashed',linewidth=1.5)
+ax[1][0].hlines(1, 1,10, colors='grey', linestyles='dashed',linewidth=1.5)
+
+
+
+ax[1][1].tick_params(labelsize=22)
+ax_ = sns.stripplot(x="Algorithms", y="Transfer Efficieny", data=df, palette=c, size=6, ax=ax[1][1])
+ax[1][1].hlines(1, -1,8, colors='grey', linestyles='dashed',linewidth=1.5)
+sns.boxplot(x="Algorithms", y="Transfer Efficieny", data=mean_df, palette=c, linewidth=3, ax=ax[1][1])
+ax_.set_yticks([.4,.6,.8,1, 1.2,1.4])
+ax_.set_xlabel('Algorithms', fontsize=fontsize)
+ax_.set_ylabel('Final Transfer Efficiency', fontsize=fontsize)
+ax_.set_xticklabels(
+    ['L2F(cap.)','L2F','L2N','Prog-NN','DF-CNN','EWC','O-EWC','SI','LwF'],
+    fontsize=12,rotation=45,ha="right",rotation_mode='anchor'
+    )
+
+right_side = ax[0][0].spines["right"]
+right_side.set_visible(False)
+top_side = ax[0][0].spines["top"]
+top_side.set_visible(False)
+
+right_side = ax[0][1].spines["right"]
+right_side.set_visible(False)
+top_side = ax[0][1].spines["top"]
+top_side.set_visible(False)
+
+right_side = ax[1][0].spines["right"]
+right_side.set_visible(False)
+top_side = ax[1][0].spines["top"]
+top_side.set_visible(False)
+
+right_side = ax[1][1].spines["right"]
+right_side.set_visible(False)
+top_side = ax[1][1].spines["top"]
+top_side.set_visible(False)
+
+plt.tight_layout()
+# lgd = fig.legend(algos, bbox_to_anchor=(1, 0.45), loc='center left', fontsize=18)
+plt.savefig('result/figs/cifar_plot.pdf', dpi=500)
+
 # %%
