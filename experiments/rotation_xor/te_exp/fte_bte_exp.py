b260c9ff60ac662b6f4523467c8fc2d7f7a296ea
Ubuntu <ubuntu@ip-172-31-29-67.us-east-2.compute.internal>
2020-06-14 22:40:06 +0000

    finished rotation experiment

--- experiments/rotation_xor/te_exp/fte_bte_exp.py
+++ experiments/rotation_xor/te_exp/fte_bte_exp.py
@@ -16,72 +16,43 @@ from lifelong_dnn import LifeLongDNN
 from joblib import Parallel, delayed
 from multiprocessing import Pool
 
-def generate_data(task_ii, num_task_1_data):
-    if task_ii == 0:
-        X_train, y_train = generate_gaussian_parity(n = num_task_1_data, angle_params = task_to_angle_map[task_ii])
-    else:
-        X_train, y_train = generate_gaussian_parity(n = 100, angle_params = task_to_angle_map[task_ii])
-
-    X_test, y_test = generate_gaussian_parity(n = 10000, angle_params = task_to_angle_map[task_ii], acorn = 0)
-    
-    return X_train, X_test, y_train, y_test
-    
 #%%
 def LF_experiment(num_task_1_data, rep):
-       
+    
+    l2f = LifeLongDNN(model = "uf", parallel = True)
+    
+    X_train_task0, y_train_task0 = generate_gaussian_parity(n = num_task_1_data, angle_params = 0)
+    X_train_task1, y_train_task1 = generate_gaussian_parity(n = 100, angle_params = 10)
+    X_test_task0, y_test_task0 = generate_gaussian_parity(n = 1000, angle_params = 0)
+    
+
+    l2f.new_forest(
+        X_train_task0, 
+        y_train_task0, 
+        n_estimators=10)
+    llf_task=l2f.predict(X_test_task0, representation=0, decider=0)
+    single_task_accuracy = np.mean(llf_task == y_test_task0)
+    single_task_error = 1 - single_task_accuracy
+
+
+    l2f.new_forest(
+        X_train_task1, 
+        y_train_task1, 
+        n_estimators=10
+        )
+    
+    llf_task=l2f.predict(X_test_task0, representation="all", decider=0)
+    double_task_accuracy = np.mean(llf_task == y_test_task0)
+    double_task_error = 1 - double_task_accuracy
+    
+    te = single_task_error / double_task_error
+
     df = pd.DataFrame()
-    single_task_accuracies = np.zeros(10,dtype=float)
-    tasks = []
-    base_tasks = []
-    reps = []
-    accuracies_across_tasks = []
+    df['te'] = [te]
 
-    for task_ii in range(10):
-        X_train, X_test, y_train, y_test = generate_data(task_ii, num_task_1_data)
-           
-        single_task_learner = LifeLongDNN(model = "uf", parallel = True)
-
-        single_task_learner.new_forest(X_train, y_train, n_estimators=10)
-        llf_task=single_task_learner.predict(X_test, representation=0, decider=0)
-        single_task_accuracies[task_ii] = np.mean(llf_task == y_test)
-
-    lifelong_forest = LifeLongDNN(model = model, parallel = True if model == "uf" else False)
-    for task_ii in range(10):
-        print("Starting Task {}".format(task_ii))
-            
-        X_train, _, y_train, _ = generate_data(task_ii, num_task_1_data)
-
-        lifelong_forest.new_forest(
-            X_train, 
-            y_train, 
-            n_estimators=10
-            )
-        
-        for task_jj in range(task_ii+1):
-            _, X_test, _, y_test = generate_data(task_jj, num_task_1_data)
-            llf_task=lifelong_forest.predict(
-                X_test, representation='all', decider=task_jj
-                )
-            
-            tasks.append(task_jj+1)
-            base_tasks.append(task_ii+1)
-            accuracies_across_tasks.append(np.mean(
-                llf_task == y_test
-                ))
-            reps.append(rep)
-            
-    df['task'] = tasks
-    df['base_task'] = base_tasks
-    df['accuracy'] = accuracies_across_tasks
-
-    df_single_task = pd.DataFrame()
-    df_single_task['task'] = range(1, 11)
-    df_single_task['accuracy'] = single_task_accuracies
-
-    summary = (df,df_single_task)
-    file_to_save = 'result/result/'+str(num_task_1_data)+'_'+str(rep)+'.pickle'
+    file_to_save = 'result/'+str(num_task_1_data)+'_'+str(rep)+'.pickle'
     with open(file_to_save, 'wb') as f:
-        pickle.dump(summary, f)
+        pickle.dump(df, f)
 
 #%%
 def generate_gaussian_parity(n, mean=np.array([0, 0]), cov_scale=1, angle_params=None, k=1, acorn=None):
@@ -105,16 +76,12 @@ def generate_gaussian_parity(n, mean=np.array([0, 0]), cov_scale=1, angle_params
 model = "uf"
 ########################
 
-task_to_angle_map = {}
-task_to_angle_map[0] = 0
-for task, angle in enumerate(np.arange(22.5, 45 + (45 - 22.5) / 9, (45 - 22.5) / 9)):
-    task_to_angle_map[task] = angle
 
 #%%
-num_task_1_data_ra=[50, 100, 200, 500]
-reps = range(10)
+num_task_1_data_ra=range(10, 1000, 10)
+reps = range(30)
 iterable = product(num_task_1_data_ra, reps)
-Parallel(n_jobs=-2,verbose=1)(delayed(LF_experiment)(num_task_1_data, rep) for num_task_1_data, rep in iterable)
+Parallel(n_jobs=-1,verbose=1)(delayed(LF_experiment)(num_task_1_data, rep) for num_task_1_data, rep in iterable)
 
     
         
