2d5d5adf2aa741802028e39ec82a5b9e6fb4c038
jdey4 <jdey4@jhmi.edu>
2020-06-16 00:08:53 -0400

    figure edit

---
+++ experiments/cifar_exp/appendix_plot.py
@@ -0,0 +1,245 @@
+#%%
+import pickle
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+import numpy as np
+import pandas as pd
+from itertools import product
+import seaborn as sns
+import matplotlib.gridspec as gridspec
+
+#%%
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def get_fte_bte(err, single_err):
+    bte = [[] for i in range(10)]
+    te = [[] for i in range(10)]
+    fte = []
+    
+    for i in range(10):
+        for j in range(i,10):
+            #print(err[j][i],j,i)
+            bte[i].append(err[i][i]/err[j][i])
+            te[i].append(single_err[i]/err[j][i])
+                
+    for i in range(10):
+        fte.append(single_err[i]/err[i][i])
+            
+            
+    return fte,bte,te
+
+def calc_mean_bte(btes,task_num=10,reps=6):
+    mean_bte = [[] for i in range(task_num)]
+
+
+    for j in range(task_num):
+        tmp = 0
+        for i in range(reps):
+            tmp += np.array(btes[i][j])
+        
+        tmp=tmp/reps
+        mean_bte[j].extend(tmp)
+            
+    return mean_bte     
+
+def calc_mean_te(tes,task_num=10,reps=6):
+    mean_te = [[] for i in range(task_num)]
+
+    for j in range(task_num):
+        tmp = 0
+        for i in range(reps):
+            tmp += np.array(tes[i][j])
+        
+        tmp=tmp/reps
+        mean_te[j].extend(tmp)
+            
+    return mean_te 
+
+def calc_mean_fte(ftes,task_num=10,reps=6):
+    fte = np.asarray(ftes)
+    
+    return list(np.mean(np.asarray(fte),axis=0))
+
+def get_error_matrix(filename):
+    multitask_df, single_task_df = unpickle(filename)
+
+    err = [[] for _ in range(10)]
+
+    for ii in range(10):
+        err[ii].extend(
+            1 - np.array(
+                multitask_df[multitask_df['base_task']==ii+1]['accuracy']
+                )
+            )
+    single_err = 1 - np.array(single_task_df['accuracy'])
+
+    return single_err, err
+
+def stratified_scatter(te_dict,axis_handle,s,color):
+    algo = list(te_dict.keys())
+    total_alg = len(algo)
+    total_points = len(te_dict[algo[0]])
+
+    pivot_points = np.arange(-.25, (total_alg+1)*1, step=1)
+    interval = .7/(total_points-1)
+
+    for algo_no,alg in enumerate(algo):
+        for no,points in enumerate(te_dict[alg]):
+            axis_handle.scatter(
+                pivot_points[algo_no]+interval*no,
+                te_dict[alg][no],
+                s=s,
+                c=color[algo_no]
+                )
+
+   
+
+#%%
+### MAIN HYPERPARAMS ###
+ntrees = 10
+slots = 10
+task_num = 10
+shifts = 6
+total_alg = 8
+alg_name = ['L2N','L2F','Prog-NN', 'DF-CNN','LwF','EWC','O-EWC','SI']
+model_file_5000 = ['dnn0','fixed_uf5000_40','Prog_NN','DF_CNN', 'LwF','EWC', 'Online_EWC', 'SI']
+
+btes_5000 = [[] for i in range(total_alg)]
+ftes_5000 = [[] for i in range(total_alg)]
+tes_5000 = [[] for i in range(total_alg)]
+########################
+
+#%% code for 5000 samples
+reps = shifts
+
+for alg in range(total_alg): 
+    count = 0 
+    te_tmp = [[] for _ in range(reps)]
+    bte_tmp = [[] for _ in range(reps)]
+    fte_tmp = [[] for _ in range(reps)] 
+
+    for shift in range(shifts):
+        if alg < 2:
+            filename = 'result/result/'+model_file_5000[alg]+'_'+str(shift+1)+'_0'+'.pickle'
+        else:
+            filename = 'benchmarking_algorthms_result/'+model_file_5000[alg]+'_'+str(shift+1)+'.pickle'
+
+        multitask_df, single_task_df = unpickle(filename)
+
+        single_err, err = get_error_matrix(filename)
+        fte, bte, te = get_fte_bte(err,single_err)
+            
+        te_tmp[count].extend(te)
+        bte_tmp[count].extend(bte)
+        fte_tmp[count].extend(fte)
+        count+=1
+    
+    tes_5000[alg].extend(calc_mean_te(te_tmp,reps=reps))
+    btes_5000[alg].extend(calc_mean_bte(bte_tmp,reps=reps))
+    ftes_5000[alg].extend(calc_mean_fte(fte_tmp,reps=reps))
+
+#%%
+clr = ["#00008B", "#e41a1c", "#a65628", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#CCCC00"]
+c = sns.color_palette(clr, n_colors=len(clr))
+
+fontsize=24
+ticksize=20
+
+fig, ax = plt.subplots(1,2, figsize=(16,6))
+for i, fte in enumerate(ftes_5000):
+    if i == 0:
+        ax[0].plot(np.arange(1,11), fte, color=clr[i], marker='.', markersize=12, label=alg_name[i], linewidth=3)
+        continue
+
+    if i == 1:
+        ax[0].plot(np.arange(1,11), fte, color=clr[i], marker='.', markersize=12, label=alg_name[i], linewidth=3)
+        continue
+    
+    ax[0].plot(np.arange(1,11), fte, color=clr[i], marker='.', markersize=12, label=alg_name[i])
+    
+ax[0].set_xticks(np.arange(1,11))
+ax[0].set_yticks([0.9, 1, 1.1, 1.2, 1.3,1.4])
+ax[0].set_ylim(0.9, 1.37)
+ax[0].tick_params(labelsize=ticksize)
+# ax[0].legend(algos, loc='upper left', fontsize=14)
+# ax[0].legend(algos, bbox_to_anchor=(1.2, -.2), loc=2, borderaxespad=0)
+
+ax[0].set_ylabel('Forward Transfer Efficiency', fontsize=fontsize)
+ax[0].set_xlabel('Number of tasks seen', fontsize=fontsize)
+
+#ax[0][0].grid(axis='x')
+
+for i in range(task_num - 1):
+
+    et = np.zeros((total_alg,task_num-i))
+
+    for j in range(0,total_alg):
+        et[j,:] = np.asarray(btes_5000[j][i])
+
+    ns = np.arange(i + 1, task_num + 1)
+    for j in range(0,total_alg):
+        if j == 0:
+            if i == 0:
+                ax[1].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], color=clr[j], linewidth = 3)
+            else:
+                ax[1].plot(ns, et[j,:], marker='.', markersize=8, color=clr[j], linewidth = 3)
+        elif j == 1:
+            if i == 0:
+                ax[1].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], color=clr[j], linewidth = 3)
+            else:
+                ax[1].plot(ns, et[j,:], marker='.', markersize=8, color=clr[j], linewidth = 3)
+        elif j==2:
+            if i == 0:
+                ax[1].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], color=clr[j], linestyle='dashed', linewidth = 3)
+            else:
+                ax[1].plot(ns, et[j,:], marker='.', markersize=8, color=clr[j], linestyle='dashed', linewidth = 3)
+        else:
+            if i == 0:
+                ax[1].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], color=clr[j])
+            else:
+                ax[1].plot(ns, et[j,:], marker='.', markersize=8, color=clr[j])
+
+
+# ax[1].set_title(ttle, fontsize=20)
+ax[1].set_xlabel('Number of tasks seen', fontsize=fontsize)
+ax[1].set_ylabel('Backward Transfer Efficiency', fontsize=fontsize)
+# ax.set_ylim(0.05 - 0.01, 0.5 + 0.01)
+# box = ax.get_position()
+# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# ax[1].legend(loc='upper left', fontsize=12)
+#ax[0][1].legend(loc='center left', bbox_to_anchor=(1,0.5), fontsize=22)
+ax[1].set_yticks([.4,.6,.8,.9,1, 1.1,1.2])
+ax[1].set_xticks(np.arange(1,11))
+ax[1].set_ylim(0.85, 1.19)
+ax[1].tick_params(labelsize=ticksize)
+#ax[0][1].grid(axis='x')
+
+right_side = ax[0].spines["right"]
+right_side.set_visible(False)
+top_side = ax[0].spines["top"]
+top_side.set_visible(False)
+
+right_side = ax[1].spines["right"]
+right_side.set_visible(False)
+top_side = ax[1].spines["top"]
+top_side.set_visible(False)
+
+ax[0].hlines(1, 1,10, colors='grey', linestyles='dashed',linewidth=1.5)
+ax[1].hlines(1, 1,10, colors='grey', linestyles='dashed',linewidth=1.5)
+
+#plt.tight_layout()
+
+#ax[0][1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.3),
+#          fancybox=True, shadow=True, ncol=3,fontsize=15)
+ax[1].legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=18)
+# lgd = fig.legend(algos, bbox_to_anchor=(1, 0.45), loc='center left', fontsize=18)
+plt.savefig('result/figs/benchmark_5000.pdf', dpi=500)
+
+
+
+# %%
