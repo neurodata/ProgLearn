3be0fe12b80435053e88ebfaa9a74768997fab6e
jdey4 <jdey4@jhmi.edu>
2020-06-17 17:17:51 -0400

    batch exp

---
+++ experiments/batch_exp/experiment.py
@@ -0,0 +1,175 @@
+#%%
+import random
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow.keras as keras
+from itertools import product
+import pandas as pd
+
+import numpy as np
+import pickle
+
+from sklearn.model_selection import StratifiedKFold
+from math import log2, ceil 
+
+import sys
+sys.path.append("../../src/")
+from lifelong_dnn import LifeLongDNN
+from joblib import Parallel, delayed
+from multiprocessing import Pool
+
+import tensorflow as tf
+
+#%%
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+    
+#%%
+def LF_experiment(train_x, train_y, test_x, test_y, ntrees, shift, slot, num_points_per_task, acorn=None):
+       
+    uf_accuracies = np.zeros(10,dtype=float)
+    single_task_accuracies = np.zeros(10,dtype=float)
+    l2f_accuracies = np.zeros(10,dtype=float)
+
+    for task_ii in range(10):
+        single_task_learner = LifeLongDNN(model = "uf", parallel = True)
+
+        if acorn is not None:
+            np.random.seed(acorn)
+
+        single_task_learner.new_forest(
+            train_x[task_ii*5000+slot*num_points_per_task:task_ii*5000+(slot+1)*num_points_per_task,:],
+            train_y[task_ii*5000+slot*num_points_per_task:task_ii*5000+(slot+1)*num_points_per_task], 
+            max_depth=ceil(log2(num_points_per_task)), n_estimators=ntrees
+            )
+        llf_task=single_task_learner.predict(
+            test_x[task_ii*1000:(task_ii+1)*1000,:], representation=0, decider=0
+            )
+        single_task_accuracies[task_ii] = np.mean(
+                llf_task == test_y[task_ii*1000:(task_ii+1)*1000]
+                )
+
+    lifelong_forest = LifeLongDNN(model = "uf", parallel = True)
+    for task_ii in range(10):
+        print("Starting L2F Task {} For Fold {}".format(task_ii, shift))
+        if acorn is not None:
+            np.random.seed(acorn)
+
+        lifelong_forest.new_forest(
+            train_x[task_ii*5000+slot*num_points_per_task:task_ii*5000+(slot+1)*num_points_per_task,:], 
+            train_y[task_ii*5000+slot*num_points_per_task:task_ii*5000+(slot+1)*num_points_per_task], 
+            max_depth=ceil(log2(num_points_per_task)), n_estimators=ntrees
+            )
+        
+        llf_task=lifelong_forest.predict(
+                test_x[task_ii*1000:(task_ii+1)*1000,:], representation='all', decider=task_ii
+                )
+        l2f_accuracies[task_ii] = np.mean(
+                llf_task == test_y[task_ii*1000:(task_ii+1)*1000]
+                )
+
+        
+    for task_ii in range(10):
+        print("Starting UF Task {} For Fold {}".format(task_ii, shift))
+        lifelong_forest = LifeLongDNN(model = "uf", parallel = True)
+        if acorn is not None:
+            np.random.seed(acorn)
+
+        if task_ii== 0:
+            train_data_x = train_x[task_ii*5000+slot*num_points_per_task:task_ii*5000+(slot+1)*num_points_per_task,:]
+            train_data_y = train_y[task_ii*5000+slot*num_points_per_task:task_ii*5000+(slot+1)*num_points_per_task]
+        else:
+            train_data_x = np.concatenate(
+                (
+                    train_data_x,
+                    train_x[task_ii*5000+slot*num_points_per_task:task_ii*5000+(slot+1)*num_points_per_task,:]
+                ),
+                axis = 0
+            )
+            train_data_y = np.concatenate(
+                (
+                    train_data_y,
+                    train_y[task_ii*5000+slot*num_points_per_task:task_ii*5000+(slot+1)*num_points_per_task]
+                ),
+                axis = 0
+            )
+        lifelong_forest.new_forest(
+            train_data_x, 
+            train_data_y, 
+            max_depth=ceil(log2(num_points_per_task*(task_ii+1))), n_estimators=(task_ii+1)*ntrees
+            )
+        
+        llf_task=lifelong_forest.predict(
+                test_x[task_ii*1000:(task_ii+1)*1000,:], representation=0, decider=0
+                )
+        uf_accuracies[task_ii] = np.mean(
+                llf_task == test_y[task_ii*1000:(task_ii+1)*1000]
+                )
+
+    return single_task_accuracies,uf_accuracies,l2f_accuracies
+
+#%%
+def cross_val_data(data_x, data_y, num_points_per_task, total_task=10, shift=1):
+    x = data_x.copy()
+    y = data_y.copy()
+    idx = [np.where(data_y == u)[0] for u in np.unique(data_y)]
+    
+    batch_per_task=5000//num_points_per_task
+    sample_per_class = num_points_per_task//total_task
+    test_data_slot=100//batch_per_task
+
+    for task in range(total_task):
+        for batch in range(batch_per_task):
+            for class_no in range(task*10,(task+1)*10,1):
+                indx = np.roll(idx[class_no],(shift-1)*100)
+                
+                if batch==0 and class_no==0 and task==0:
+                    train_x = x[indx[batch*sample_per_class:(batch+1)*sample_per_class],:]
+                    train_y = y[indx[batch*sample_per_class:(batch+1)*sample_per_class]]
+                    test_x = x[indx[batch*test_data_slot+500:(batch+1)*test_data_slot+500],:]
+                    test_y = y[indx[batch*test_data_slot+500:(batch+1)*test_data_slot+500]]
+                else:
+                    train_x = np.concatenate((train_x, x[indx[batch*sample_per_class:(batch+1)*sample_per_class],:]), axis=0)
+                    train_y = np.concatenate((train_y, y[indx[batch*sample_per_class:(batch+1)*sample_per_class]]), axis=0)
+                    test_x = np.concatenate((test_x, x[indx[batch*test_data_slot+500:(batch+1)*test_data_slot+500],:]), axis=0)
+                    test_y = np.concatenate((test_y, y[indx[batch*test_data_slot+500:(batch+1)*test_data_slot+500]]), axis=0)
+            
+    return train_x, train_y, test_x, test_y
+
+#%%
+def run_parallel_exp(data_x, data_y, n_trees, num_points_per_task, slot=0, shift=1):
+    train_x, train_y, test_x, test_y = cross_val_data(data_x, data_y, num_points_per_task, shift=shift)
+    
+    single_task_accuracies,uf_accuracies,l2f_accuracies = LF_experiment(train_x, train_y, test_x, test_y, n_trees, shift, slot, num_points_per_task, acorn=12345)
+
+    return single_task_accuracies,uf_accuracies,l2f_accuracies
+#%%
+### MAIN HYPERPARAMS ###
+model = "uf"
+num_points_per_task = 500
+########################
+
+(X_train, y_train), (X_test, y_test) = keras.datasets.cifar100.load_data()
+data_x = np.concatenate([X_train, X_test])
+data_x = data_x.reshape((data_x.shape[0], data_x.shape[1] * data_x.shape[2] * data_x.shape[3]))
+data_y = np.concatenate([y_train, y_test])
+data_y = data_y[:, 0]
+
+
+#%%
+slot_fold = range(5000//num_points_per_task)
+shift_fold = range(1,7,1)
+n_trees=[10]
+iterable = product(n_trees,shift_fold,slot_fold)
+
+res = Parallel(n_jobs=10,verbose=1)(
+    delayed(run_parallel_exp)(
+            data_x, data_y, ntree, num_points_per_task, slot=slot, shift=shift
+            ) for ntree,shift,slot in iterable
+        )
+
+with open('result/res.pickle','wb') as f:
+    pickle.dump(res,f)      
+# %%
---
+++ experiments/batch_exp/plotting.py
@@ -0,0 +1,93 @@
+#%%
+import random
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow.keras as keras
+from itertools import product
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import pickle
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+# %%
+file_to_load = 'result/res.pickle'
+res = unpickle(file_to_load)
+reps = len(res)
+
+task_acc_single = np.zeros((10,reps),dtype=float)
+task_acc_uf = np.zeros((10,reps),dtype=float)
+task_acc_l2f= np.zeros((10,reps),dtype=float)
+
+for i in range(reps):
+    task_acc_single[:,i], task_acc_uf[:,i], task_acc_l2f[:,i] = res[i]
+
+#%%
+fontsize = 24
+ticksize = 20
+fig, ax = plt.subplots(1,1, figsize=(8,8))
+mean_acc = np.mean(task_acc_single,axis=1)
+std_acc = np.std(task_acc_single,axis=1,ddof=1)
+
+colors = sns.color_palette('Set1', n_colors=3)
+    
+ax.plot(np.arange(1, 11), mean_acc, c=colors[0], label='UF fixed tree')
+ax.fill_between(np.arange(1, 11), 
+        mean_acc+ 1.96*np.array(std_acc), 
+        mean_acc - 1.96*np.array(std_acc), 
+        where=mean_acc + 1.96*np.array(std_acc) >= mean_acc - 1.96*np.array(std_acc), 
+        facecolor=colors[0], 
+        alpha=0.15,
+        interpolate=False)
+
+
+mean_acc = np.mean(task_acc_uf,axis=1)
+std_acc = np.std(task_acc_uf,axis=1,ddof=1)
+
+colors = sns.color_palette('Set1', n_colors=3)
+    
+ax.plot(np.arange(1, 11), mean_acc, c=colors[1], label='UF increasing tree')
+ax.fill_between(np.arange(1, 11), 
+        mean_acc+ 1.96*np.array(std_acc), 
+        mean_acc - 1.96*np.array(std_acc), 
+        where=mean_acc + 1.96*np.array(std_acc) >= mean_acc - 1.96*np.array(std_acc), 
+        facecolor=colors[1], 
+        alpha=0.15,
+        interpolate=False)
+
+
+mean_acc = np.mean(task_acc_l2f,axis=1)
+std_acc = np.std(task_acc_l2f,axis=1,ddof=1)
+
+colors = sns.color_palette('Set1', n_colors=3)
+    
+ax.plot(np.arange(1, 11), mean_acc, c=colors[2], label='L2F')
+ax.fill_between(np.arange(1, 11), 
+        mean_acc+ 1.96*np.array(std_acc), 
+        mean_acc - 1.96*np.array(std_acc), 
+        where=mean_acc + 1.96*np.array(std_acc) >= mean_acc - 1.96*np.array(std_acc), 
+        facecolor=colors[2], 
+        alpha=0.15,
+        interpolate=False)
+
+#ax.set_title('CIFAR Recruitment Experiment', fontsize=30)
+ax.set_ylabel('Accuracy', fontsize=fontsize)
+ax.set_xlabel('Task Number', fontsize=fontsize)
+ax.tick_params(labelsize=ticksize)
+#ax.set_ylim(0.325, 0.575)
+#ax.set_title("CIFAR Recruitment",fontsize=titlesize)
+ax.set_xticks([1,2,3,4,5,6,7,8,9,10])
+ax.set_yticks([0.1,0.2, 0.3, 0.4,0.5])
+
+ax.legend(loc='upper left',fontsize=12,frameon=False)
+
+right_side = ax.spines["right"]
+right_side.set_visible(False)
+top_side = ax.spines["top"]
+top_side.set_visible(False)
+
+plt.savefig('result/batch_exp.pdf')
+# %%
