ab926735a89bfa8be0a863e4adfc004ba0f555e0
jdey4 <jdey4@jhmi.edu>
2020-06-28 13:06:48 -0400

    figure edit

--- experiments/xor_rxor_spiral_exp/main_fig_plot.py
+++ experiments/xor_rxor_spiral_exp/main_fig_plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.keras as keras
 import seaborn as sns 
-
+import matplotlib
 import numpy as np
 import pickle
 
@@ -301,7 +301,7 @@ top_side.set_visible(False)
 
 ax = fig.add_subplot(gs[23:29,12:19])
 te_ra = []
-n1_ra = range(10, 6000, 50)
+n1_ra = range(10, 5000, 50)
 for n1 in n1_ra:
     te_across_reps = []
     for rep in range(500):
@@ -311,8 +311,16 @@ for n1 in n1_ra:
     te_ra.append(np.mean(te_across_reps))
 
 ax.plot(n1_ra, te_ra, c="#e41a1c", linewidth = 2.6)
-ax.tick_params(labelsize=labelsize)
-ax.set_xticks([100, 1000, 2500, 5000])
+ax.set_xscale('log')
+ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+ax.set_xticks([])
+#ax.tick_params(labelsize=labelsize)
+#ax.set_xticks([100, 1000, 2500, 5000])
+ax.text(100, 0.434, "100", fontsize=labelsize)
+ax.text(500, 0.434, "1000", fontsize=labelsize)
+ax.text(5000, 0.434, "2500", fontsize=labelsize)
+ax.text(5000, 0.434, "5000", fontsize=labelsize)
+
 ax.hlines(1, 1, max(n1_ra), colors='grey', linestyles='dashed',linewidth=1.5)
 ax.set_xlabel('Number of Task 1 Training Samples', fontsize=fontsize)
 ax.set_ylabel('Backward Transfer Efficiency', fontsize=fontsize)
