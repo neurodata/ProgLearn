dfdf16540f4c4bbea2e8a72e05b69a921850e523
j1c <jdey4@jhmi.edu>
2020-09-02 17:28:35 -0400

    adding readme

--- setup.py
+++ setup.py
@@ -9,10 +9,8 @@ requirements = [
     "joblib",
 ]
 
-# Uncomment once README.md exists
-
-# with open("README.md", "r") as readme_file:
-#     readme = readme_file.read()
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
 
 
 
@@ -22,9 +20,10 @@ setup(
     author="Will Levine, Jayanta Dey, Hayden Helm",
     author_email="levinewill@icloud.com",
     description="A package to implement and extend the methods desribed in 'A General Approach to Progressive Learning'",
-    # long_description=readme,
-    # long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     url="https://github.com/neurodata/progressive-learning/",
+    license="Apache 2.0",
     packages=find_packages(),
     install_requires=requirements,
 )
