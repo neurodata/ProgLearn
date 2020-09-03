a8168b629b2d5b6b1e64b697685161d3549d1466
j1c <jdey4@jhmi.edu>
2020-09-03 15:02:46 -0400

    system test

--- proglearn/progressive_learner.py
+++ proglearn/progressive_learner.py
@@ -1,5 +1,5 @@
 import numpy as np
-from base import ClassificationDecider, ClassificationProgressiveLearner
+from .base import ClassificationDecider, ClassificationProgressiveLearner
 
 
 class ProgressiveLearner(ClassificationProgressiveLearner):
