18002521f5009b4d72ced0df99d4ce80e0a92c48
Will LeVine <levinewill@icloud.com>
2020-09-09 17:48:14 -0400

    Purged regression code from transformers.py

--- proglearn/transformers.py
+++ proglearn/transformers.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.utils.validation import (
     check_X_y,
@@ -108,122 +108,6 @@ class TreeClassificationTransformer(BaseTransformer):
 
         # define the ensemble
         self.transformer = DecisionTreeClassifier(**self.kwargs).fit(X, y)
-
-        self._is_fitted = True
-
-        return self
-
-    def transform(self, X):
-        """
-        Doc strings here.
-        """
-
-        if not self.is_fitted():
-            msg = (
-                "This %(name)s instance is not fitted yet. Call 'fit' with "
-                "appropriate arguments before using this transformer."
-            )
-            raise NotFittedError(msg % {"name": type(self).__name__})
-
-        X = check_array(X)
-        return self.transformer.apply(X)
-
-    def is_fitted(self):
-        """
-        Doc strings here.
-        """
-
-        return self._is_fitted
-
-
-class NeuralRegressionTransformer(BaseTransformer):
-    def __init__(
-        self,
-        network,
-        optimizer="adam",
-        euclidean_layer_idx=-2,
-        loss="mse",
-        pretrained=False,
-        compile_kwargs={"metrics": ["mae", "mape"]},
-        fit_kwargs={
-            "epochs": 100,
-            "callbacks": [
-                keras.callbacks.EarlyStopping(patience=5, monitor="val_loss")
-            ],
-            "verbose": False,
-            "validation_split": 0.25,
-        },
-    ):
-        """
-        Doc strings here.
-        """
-        self.network = keras.models.clone_model(network)
-        self.encoder = keras.models.Model(
-            inputs=self.network.inputs,
-            outputs=self.network.layers[euclidean_layer_idx].output,
-        )
-        self._is_fitted = pretrained
-        self.optimizer = optimizer
-        self.loss = loss
-        self.compile_kwargs = compile_kwargs
-        self.fit_kwargs = fit_kwargs
-
-    def fit(self, X, y):
-        """
-        Doc strings here.
-        """
-        X, y = check_X_y(X, y)
-
-        self.network.compile(
-            loss=self.loss, optimizer=self.optimizer, **self.compile_kwargs
-        )
-        self.network.fit(X, y, **self.fit_kwargs)
-        self._is_fitted = True
-
-        return self
-
-    def transform(self, X):
-        """
-        Doc strings here.
-        """
-
-        if not self.is_fitted():
-            msg = (
-                "This %(name)s instance is not fitted yet. Call 'fit' with "
-                "appropriate arguments before using this transformer."
-            )
-            raise NotFittedError(msg % {"name": type(self).__name__})
-
-        X = check_array(X)
-        return self.encoder.predict(X)
-
-    def is_fitted(self):
-        """
-        Doc strings here.
-        """
-
-        return self._is_fitted
-
-
-class TreeRegressionTransformer(BaseTransformer):
-    def __init__(self, kwargs={}):
-        """
-        Doc strings here.
-        """
-
-        self.kwargs = kwargs
-
-        self._is_fitted = False
-
-    def fit(self, X, y):
-        """
-        Doc strings here.
-        """
-
-        X, y = check_X_y(X, y)
-
-        # define the ensemble
-        self.transformer = DecisionTreeRegressor(**self.kwargs).fit(X, y)
 
         self._is_fitted = True
 
