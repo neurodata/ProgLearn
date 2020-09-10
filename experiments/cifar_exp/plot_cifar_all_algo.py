dfd2d7fd4b68c68b3f08e384c0d5a407d8374df6
j1c <jdey4@jhmi.edu>
2020-09-10 12:47:11 -0400

    get rid of pickle files

---
+++ experiments/cifar_exp/plot_cifar_all_algo.py
@@ -0,0 +1,300 @@
+#%%
+import pickle
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
+import numpy as np
+import pandas as pd
+from itertools import product
+import seaborn as sns
+
+#%%
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def get_fte_bte(err, single_err, ntrees):
+    bte = [[] for i in range(10)]
+    te = [[] for i in range(10)]
+    fte = []
+    
+    for i in range(10):
+        for j in range(i,10):
+            #print(err[j][i],j,i)
+            bte[i].append(err[i][i]/err[j][i])
+            te[i].append(single_err[i]/err[j][i])
+                
+    for i in range(10):
+        fte.append(single_err[i]/err[i][i])
+            
+            
+    return fte,bte,te
+
+def calc_mean_bte(btes,task_num=10,reps=6):
+    mean_bte = [[] for i in range(task_num)]
+
+
+    for j in range(task_num):
+        tmp = 0
+        for i in range(reps):
+            tmp += np.array(btes[i][j])
+        
+        tmp=tmp/reps
+        mean_bte[j].extend(tmp)
+            
+    return mean_bte     
+
+def calc_mean_te(tes,task_num=10,reps=6):
+    mean_te = [[] for i in range(task_num)]
+
+    for j in range(task_num):
+        tmp = 0
+        for i in range(reps):
+            tmp += np.array(tes[i][j])
+        
+        tmp=tmp/reps
+        mean_te[j].extend(tmp)
+            
+    return mean_te 
+
+def calc_mean_fte(ftes,task_num=10,reps=6):
+    fte = np.asarray(ftes)
+    
+    return list(np.mean(np.asarray(fte),axis=0))
+
+def get_error_matrix(filename):
+    multitask_df, single_task_df = unpickle(filename)
+
+    err = [[] for _ in range(10)]
+
+    for ii in range(10):
+        err[ii].extend(
+            1 - np.array(
+                multitask_df[multitask_df['base_task']==ii+1]['accuracy']
+                )
+            )
+    single_err = 1 - np.array(single_task_df['accuracy'])
+
+    return single_err, err
+
+#%%
+### MAIN HYPERPARAMS ###
+ntrees = 10
+slots = 4
+task_num = 10
+shifts = 1
+total_alg = 8
+alg_name = ['L2N','L2F','Prog-NN', 'DF-CNN','LwF','EWC','O-EWC','SI']
+model_file = ['dnn0','fixed_uf10','Prog_NN','DF_CNN', 'LwF','EWC', 'Online_EWC', 'SI']
+btes = [[] for i in range(total_alg)]
+ftes = [[] for i in range(total_alg)]
+tes = [[] for i in range(total_alg)]
+########################
+
+#%% code for L2F and L2N
+reps = slots*shifts
+
+for alg in range(total_alg): 
+    count = 0 
+    te_tmp = [[] for _ in range(reps)]
+    bte_tmp = [[] for _ in range(reps)]
+    fte_tmp = [[] for _ in range(reps)] 
+
+    for slot in range(slots):
+        for shift in range(shifts):
+            if alg < 2:
+                filename = 'result/result/'+model_file[alg]+'_'+str(shift+1)+'_'+str(slot)+'.pickle'
+            else:
+                filename = 'benchmarking_algorthms_result/'+model_file[alg]+'_'+str(shift+1)+'_'+str(slot)+'.pickle'
+
+            multitask_df, single_task_df = unpickle(filename)
+
+            single_err, err = get_error_matrix(filename)
+            fte, bte, te = get_fte_bte(err,single_err,ntrees)
+            
+            te_tmp[count].extend(te)
+            bte_tmp[count].extend(bte)
+            fte_tmp[count].extend(fte)
+            count+=1
+    
+    tes[alg].extend(calc_mean_te(te_tmp,reps=reps))
+    btes[alg].extend(calc_mean_bte(bte_tmp,reps=reps))
+    ftes[alg].extend(calc_mean_fte(fte_tmp,reps=reps))
+
+#%%
+te = {'L2N':np.zeros(10,dtype=float), 'L2F':np.zeros(10,dtype=float),'Prog-NN':np.zeros(10,dtype=float), 'DF-CNN':np.zeros(10,dtype=float), 'LwF':np.zeros(10,dtype=float),'EWC':np.zeros(10,dtype=float), 'Online EWC':np.zeros(10,dtype=float), 'SI':np.zeros(10,dtype=float)}
+
+for count,name in enumerate(te.keys()):
+    for i in range(10):
+        te[name][i] = tes[count][i][9-i]
+
+df = pd.DataFrame.from_dict(te)
+df = pd.melt(df,var_name='Algorithms', value_name='Transfer Efficieny')
+
+mean_te = {'L2N':[np.mean(te['L2N'])],'L2F':[np.mean(te['L2F'])],
+            'Prog-NN':[np.mean(te['Prog-NN'])],'DF-CNN':[np.mean(te['DF-CNN'])], 
+           'LwF':[np.mean(te['LwF'])],'EWC':[np.mean(te['EWC'])], 
+           'Online EWC':[np.mean(te['Online EWC'])], 'SI':[np.mean(te['SI'])]
+           }
+mean_df = pd.DataFrame.from_dict(mean_te)
+mean_df = pd.melt(mean_df,var_name='Algorithms', value_name='Transfer Efficieny')
+
+#%%
+clr = ["#00008B", "#e41a1c", "#a65628", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#CCCC00"]
+c = sns.color_palette(clr, n_colors=len(clr))
+
+fontsize=24
+ticksize=20
+
+fig, ax = plt.subplots(2,2, figsize=(16,11.5))
+# plt.subplots_adjust(right=0.5)
+for i, fte in enumerate(ftes):
+    if i == 0:
+        ax[0][0].plot(np.arange(1,11), fte, c=clr[i], marker='.', markersize=12, label=alg_name[i], linewidth=3)
+        continue
+
+    if i == 1:
+        ax[0][0].plot(np.arange(1,11), fte, c=clr[i], marker='.', markersize=12, label=alg_name[i], linewidth=3)
+        continue
+    
+    ax[0][0].plot(np.arange(1,11), fte, c=clr[i], marker='.', markersize=12, label=alg_name[i])
+    
+ax[0][0].set_xticks(np.arange(1,11))
+ax[0][0].set_yticks([0.9, 1, 1.1, 1.2, 1.3,1.4])
+ax[0][0].set_ylim(0.85, 1.41)
+ax[0][0].tick_params(labelsize=ticksize)
+# ax[0].legend(algos, loc='upper left', fontsize=14)
+# ax[0].legend(algos, bbox_to_anchor=(1.2, -.2), loc=2, borderaxespad=0)
+
+ax[0][0].set_ylabel('Forward Transfer Efficiency', fontsize=fontsize)
+ax[0][0].set_xlabel('Number of tasks seen', fontsize=fontsize)
+
+#ax[0][0].grid(axis='x')
+
+for i in range(task_num - 1):
+
+    et = np.zeros((total_alg,task_num-i))
+
+    for j in range(0,total_alg):
+        et[j,:] = np.asarray(btes[j][i])
+
+    ns = np.arange(i + 1, task_num + 1)
+    for j in range(0,total_alg):
+        if j == 0:
+            if i == 0:
+                ax[0][1].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], c=c[j], linewidth = 3)
+            else:
+                ax[0][1].plot(ns, et[j,:], marker='.', markersize=8, c=c[j], linewidth = 3)
+        elif j == 1:
+            if i == 0:
+                ax[0][1].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], c=c[j], linewidth = 3)
+            else:
+                ax[0][1].plot(ns, et[j,:], marker='.', markersize=8, c=c[j], linewidth = 3)
+        else:
+            if i == 0:
+                ax[0][1].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], c=c[j])
+            else:
+                ax[0][1].plot(ns, et[j,:], marker='.', markersize=8, c=c[j])
+
+
+# ax[1].set_title(ttle, fontsize=20)
+ax[0][1].set_xlabel('Number of tasks seen', fontsize=fontsize)
+ax[0][1].set_ylabel('Backward Transfer Efficiency', fontsize=fontsize)
+# ax.set_ylim(0.05 - 0.01, 0.5 + 0.01)
+# box = ax.get_position()
+# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# ax[1].legend(loc='upper left', fontsize=12)
+ax[0][1].legend(loc='center left', bbox_to_anchor=(1,0.5), fontsize=22)
+ax[0][1].set_yticks([.4,.6,.8,.9,1, 1.1,1.2])
+ax[0][1].set_xticks(np.arange(1,11))
+ax[0][1].set_ylim(0.85, 1.19)
+ax[0][1].tick_params(labelsize=ticksize)
+#ax[0][1].grid(axis='x')
+
+
+for i in range(task_num- 1):
+
+    et = np.zeros((total_alg,task_num-i))
+
+    for j in range(0,total_alg):
+        et[j,:] = np.asarray(tes[j][i])
+
+    ns = np.arange(i + 1, task_num + 1)
+    for j in range(0,total_alg):
+        if j == 0:
+            if i == 0 or i==1:
+                ax[1][0].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], c=c[j], linewidth = 3)
+            else:
+                ax[1][0].plot(ns, et[j,:], marker='.', markersize=8, c=c[j], linewidth = 3)
+        elif j == 1:
+            if i == 0 or i==1:
+                ax[1][0].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], c=c[j], linewidth = 3)
+            else:
+                ax[1][0].plot(ns, et[j,:], marker='.', markersize=8, c=c[j], linewidth = 3)        
+        else:
+            if i == 0 or i==1:
+                ax[1][0].plot(ns, et[j,:], marker='.', markersize=8, label = alg_name[j], c=c[j])
+            else:
+                ax[1][0].plot(ns, et[j,:], marker='.', markersize=8, c=c[j])
+
+
+# ax[1].set_title(ttle, fontsize=20)
+ax[1][0].set_xlabel('Number of tasks seen', fontsize=fontsize)
+ax[1][0].set_ylabel('Transfer Efficiency', fontsize=fontsize)
+# ax.set_ylim(0.05 - 0.01, 0.5 + 0.01)
+# box = ax.get_position()
+# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# ax[1].legend(loc='upper left', fontsize=12)
+#ax[1][0].legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=22)
+ax[1][0].set_yticks([.4,.6,.8,.9,1, 1.1,1.2])
+ax[1][0].set_xticks(np.arange(1,11))
+ax[1][0].set_ylim(0.85, 1.26)
+ax[1][0].tick_params(labelsize=ticksize)
+#ax[1][0].grid(axis='x')
+
+ax[0][0].hlines(1, 1,10, colors='grey', linestyles='dashed',linewidth=1.5)
+ax[0][1].hlines(1, 1,10, colors='grey', linestyles='dashed',linewidth=1.5)
+ax[1][0].hlines(1, 1,10, colors='grey', linestyles='dashed',linewidth=1.5)
+
+
+
+ax[1][1].tick_params(labelsize=22)
+ax_ = sns.stripplot(x="Algorithms", y="Transfer Efficieny", data=df, palette=c, size=6, ax=ax[1][1])
+ax[1][1].hlines(1, -1,8, colors='grey', linestyles='dashed',linewidth=1.5)
+sns.boxplot(x="Algorithms", y="Transfer Efficieny", data=mean_df, palette=c, linewidth=3, ax=ax[1][1])
+ax_.set_yticks([.4,.6,.8,1, 1.2,1.4])
+ax_.set_xlabel('Algorithms', fontsize=fontsize)
+ax_.set_ylabel('Final Transfer Efficiency', fontsize=fontsize)
+ax_.set_xticklabels(
+    ['L2N','L2F','Prog-NN','DF-CNN','LwF','EWC','O-EWC','SI'],
+    fontsize=12,rotation=45,ha="right",rotation_mode='anchor'
+    )
+
+right_side = ax[0][0].spines["right"]
+right_side.set_visible(False)
+top_side = ax[0][0].spines["top"]
+top_side.set_visible(False)
+
+right_side = ax[0][1].spines["right"]
+right_side.set_visible(False)
+top_side = ax[0][1].spines["top"]
+top_side.set_visible(False)
+
+right_side = ax[1][0].spines["right"]
+right_side.set_visible(False)
+top_side = ax[1][0].spines["top"]
+top_side.set_visible(False)
+
+right_side = ax[1][1].spines["right"]
+right_side.set_visible(False)
+top_side = ax[1][1].spines["top"]
+top_side.set_visible(False)
+
+plt.tight_layout()
+# lgd = fig.legend(algos, bbox_to_anchor=(1, 0.45), loc='center left', fontsize=18)
+plt.savefig('result/figs/benchmark.pdf', dpi=500)
+
+#%%
---
+++ experiments/rotation_cifar/rotated_cifar.py
@@ -0,0 +1,200 @@
+import matplotlib.pyplot as plt
+import random
+import pickle
+from skimage.transform import rotate
+from scipy import ndimage
+from skimage.util import img_as_ubyte
+from joblib import Parallel, delayed
+from sklearn.ensemble.forest import _generate_unsampled_indices
+from sklearn.ensemble.forest import _generate_sample_indices
+import numpy as np
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from itertools import product
+import keras
+
+from keras import layers
+
+from joblib import Parallel, delayed
+from multiprocessing import Pool
+
+import tensorflow as tf
+
+from numba import cuda
+
+import sys
+sys.path.append("../../proglearn/")
+from progressive_learner import ProgressiveLearner
+from deciders import SimpleAverage
+from transformers import TreeClassificationTransformer, NeuralClassificationTransformer 
+from voters import TreeClassificationVoter, KNNClassificationVoter
+
+def cross_val_data(data_x, data_y, total_cls=10):
+    x = data_x.copy()
+    y = data_y.copy()
+    idx = [np.where(data_y == u)[0] for u in np.unique(data_y)]
+    
+    
+    for i in range(total_cls):
+        indx = idx[i]#np.roll(idx[i],(cv-1)*100)
+        random.shuffle(indx)
+        
+        if i==0:
+            train_x1 = x[indx[0:250],:]
+            train_x2 = x[indx[250:500],:]
+            train_y1 = y[indx[0:250]]
+            train_y2 = y[indx[250:500]]
+            
+            test_x = x[indx[500:600],:]
+            test_y = y[indx[500:600]]
+        else:
+            train_x1 = np.concatenate((train_x1, x[indx[0:250],:]), axis=0)
+            train_x2 = np.concatenate((train_x2, x[indx[250:500],:]), axis=0)
+            train_y1 = np.concatenate((train_y1, y[indx[0:250]]), axis=0)
+            train_y2 = np.concatenate((train_y2, y[indx[250:500]]), axis=0)
+            
+            test_x = np.concatenate((test_x, x[indx[500:600],:]), axis=0)
+            test_y = np.concatenate((test_y, y[indx[500:600]]), axis=0)
+        
+        
+    return train_x1, train_y1, train_x2, train_y2, test_x, test_y 
+
+def LF_experiment(data_x, data_y, angle, model, granularity, reps=1, ntrees=29, acorn=None):
+    if acorn is not None:
+        np.random.seed(acorn)
+    
+    errors = np.zeros(2)
+    
+    with tf.device('/gpu:'+str(int(angle //  granularity) % 4)):
+        for rep in range(reps):
+            train_x1, train_y1, train_x2, train_y2, test_x, test_y = cross_val_data(data_x, data_y, total_cls=10)
+
+
+            #change data angle for second task
+            tmp_data = train_x2.copy()
+            _tmp_ = np.zeros((32,32,3), dtype=int)
+            total_data = tmp_data.shape[0]
+
+            for i in range(total_data):
+                tmp_ = image_aug(tmp_data[i],angle)
+                tmp_data[i] = tmp_
+
+            if model == "uf":
+                train_x1 = train_x1.reshape((train_x1.shape[0], train_x1.shape[1] * train_x1.shape[2] * train_x1.shape[3]))
+                tmp_data = tmp_data.reshape((tmp_data.shape[0], tmp_data.shape[1] * tmp_data.shape[2] * tmp_data.shape[3]))
+                test_x = test_x.reshape((test_x.shape[0], test_x.shape[1] * test_x.shape[2] * test_x.shape[3]))
+
+            if model == "dnn":
+
+                default_transformer_class = NeuralClassificationTransformer
+
+                network = keras.Sequential()
+                network.add(layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu', input_shape=np.shape(train_x1)[1:]))
+                network.add(layers.BatchNormalization())
+                network.add(layers.Conv2D(filters=32, kernel_size=(3, 3), strides = 2, padding = "same", activation='relu'))
+                network.add(layers.BatchNormalization())
+                network.add(layers.Conv2D(filters=64, kernel_size=(3, 3), strides = 2, padding = "same", activation='relu'))
+                network.add(layers.BatchNormalization())
+                network.add(layers.Conv2D(filters=128, kernel_size=(3, 3), strides = 2, padding = "same", activation='relu'))
+                network.add(layers.BatchNormalization())
+                network.add(layers.Conv2D(filters=254, kernel_size=(3, 3), strides = 2, padding = "same", activation='relu'))
+
+                network.add(layers.Flatten())
+                network.add(layers.BatchNormalization())
+                network.add(layers.Dense(2000, activation='relu'))
+                network.add(layers.BatchNormalization())
+                network.add(layers.Dense(2000, activation='relu'))
+                network.add(layers.BatchNormalization())
+                network.add(layers.Dense(units=10, activation = 'softmax'))
+
+                default_transformer_kwargs = {"network" : network, 
+                                              "euclidean_layer_idx" : -2,
+                                              "num_classes" : 10,
+                                              "optimizer" : keras.optimizers.Adam(3e-4)
+                                             }
+
+                default_voter_class = KNNClassificationVoter
+                default_voter_kwargs = {"k" : int(np.log2(len(train_x1)))}
+
+                default_decider_class = SimpleAverage
+            elif model == "uf":
+                default_transformer_class = TreeClassificationTransformer
+                default_transformer_kwargs = {"kwargs" : {"max_depth" : 30}}
+
+                default_voter_class = TreeClassificationVoter
+                default_voter_kwargs = {}
+
+                default_decider_class = SimpleAverage
+
+
+            progressive_learner = ProgressiveLearner(default_transformer_class = default_transformer_class, 
+                                         default_transformer_kwargs = default_transformer_kwargs,
+                                         default_voter_class = default_voter_class,
+                                         default_voter_kwargs = default_voter_kwargs,
+                                         default_decider_class = default_decider_class)
+
+            progressive_learner.add_task(
+                X = train_x1, 
+                y = train_y1,
+                transformer_voter_decider_split = [0.67, 0.33, 0],
+                decider_kwargs = {"classes" : np.unique(train_y1)}
+            )
+
+            progressive_learner.add_transformer(
+                X = tmp_data, 
+                y = train_y2,
+                transformer_data_proportion = 1,
+                backward_task_ids = [0]
+            )
+
+
+            llf_task1=progressive_learner.predict(test_x, task_id=0)
+            llf_single_task=progressive_learner.predict(test_x, task_id=0, transformer_ids=[0])
+
+            errors[1] = errors[1]+(1 - np.mean(llf_task1 == test_y))
+            errors[0] = errors[0]+(1 - np.mean(llf_single_task == test_y))
+    
+    errors = errors/reps
+    print("Errors For Angle {}: {}".format(angle, errors))
+    with open('results/angle_'+str(angle)+'_'+model+'.pickle', 'wb') as f:
+        pickle.dump(errors, f, protocol = 2)
+        
+def image_aug(pic, angle, centroid_x=23, centroid_y=23, win=16, scale=1.45):
+    im_sz = int(np.floor(pic.shape[0]*scale))
+    pic_ = np.uint8(np.zeros((im_sz,im_sz,3),dtype=int))
+    
+    pic_[:,:,0] = ndimage.zoom(pic[:,:,0],scale)
+    
+    pic_[:,:,1] = ndimage.zoom(pic[:,:,1],scale)
+    pic_[:,:,2] = ndimage.zoom(pic[:,:,2],scale)
+    
+    image_aug = rotate(pic_, angle, resize=False)
+    #print(image_aug.shape)
+    image_aug_ = image_aug[centroid_x-win:centroid_x+win,centroid_y-win:centroid_y+win,:]
+    
+    return img_as_ubyte(image_aug_)
+
+### MAIN HYPERPARAMS ###
+model = "dnn"
+granularity = 2
+reps = 4
+########################
+
+
+(X_train, y_train), (X_test, y_test) = keras.datasets.cifar100.load_data()
+data_x = np.concatenate([X_train, X_test])
+data_y = np.concatenate([y_train, y_test])
+data_y = data_y[:, 0]
+
+def perform_angle(angle):
+    LF_experiment(data_x, data_y, angle, model, granularity, reps=reps, ntrees=16, acorn=1)
+
+if model == "dnn":
+    for angle_adder in range(0, 180, granularity * 4):
+        angles = angle_adder + np.arange(0, granularity * 4, granularity)
+        with Pool(4) as p:
+            p.map(perform_angle, angles)
+elif model == "uf":
+    angles = np.arange(0, 180,2)
+    with Pool(96) as p:
+        p.map(perform_angle, angles)
