c17bd678c4b4d74a711790c4d37386302139a053
Will LeVine <levinewill@icloud.com>
2020-09-10 14:39:26 -0400

    Matched LifelongClassificationNetwork and LifelongClassificationForest hyperparams and purged setup function in LifelongClassificationNetwork

--- proglearn/network.py
+++ proglearn/network.py
@@ -5,9 +5,9 @@ Corresponding Email: levinewill@icloud.com
 import numpy as np
 
 from .progressive_learner import ProgressiveLearner
-from .transformers import NeuralClassificationTransformer, NeuralRegressionTransformer
-from .voters import KNNClassificationVoter, NeuralRegressionVoter
-from .deciders import SimpleAverage, LinearRegressionDecider, KNNRegressionDecider
+from .transformers import NeuralClassificationTransformer
+from .voters import KNNClassificationVoter
+from .deciders import SimpleAverage
 
 from sklearn.utils import check_X_y, check_array
 from keras.optimizers import Adam
@@ -29,58 +29,43 @@ class LifelongClassificationNetwork:
         self.optimizer = optimizer
         self.verbose = verbose
         self.batch_size = batch_size
-        self.is_first_task = True
-
-    def setup(self, num_points_per_task):
-
+        
         # Set transformer network hyperparameters.
         default_transformer_kwargs = {
             "network": self.network,
             "euclidean_layer_idx": -2,
             "loss": self.loss,
             "optimizer": self.optimizer,
-            "num_classes": 10,
-            "compile_kwargs": {},
             "fit_kwargs": {
                 "epochs": self.epochs,
-                # "callbacks": [EarlyStopping(patience=5, monitor="val_loss")],
+                "callbacks": [EarlyStopping(patience=5, monitor="val_loss")],
                 "verbose": self.verbose,
                 "validation_split": 0.33,
                 "batch_size": self.batch_size
             },
         }
 
-        # Hyperparameter for KNN voter.
-        default_voter_kwargs = {"k": int(np.log2(num_points_per_task))}
-
         self.pl = ProgressiveLearner(
             default_transformer_class=NeuralClassificationTransformer,
             default_transformer_kwargs=default_transformer_kwargs,
             default_voter_class=KNNClassificationVoter,
-            default_voter_kwargs=default_voter_kwargs,
+            default_voter_kwargs={},
             default_decider_class=SimpleAverage,
             default_decider_kwargs={},
         )
 
-    def add_task(self, X, y, task_id=None, decider_kwargs={}):
-
-        if self.is_first_task:
-            num_points_per_task = len(X)
-            self.setup(num_points_per_task)
-            self.is_first_task = False
-
+    def add_task(self, X, y, task_id=None, transformer_voter_decider_split=[0.67, 0.33, 0]):
         self.pl.add_task(
             X,
             y,
             task_id=task_id,
-            transformer_voter_decider_split=[0.67, 0.33, 0.0],
-            decider_kwargs=decider_kwargs,
+            transformer_voter_decider_split=transformer_voter_decider_split
         )
 
         return self
 
-    def predict(self, X, task_id):
-        return self.pl.predict(X, task_id)
+    def predict(self, X, task_id, transformer_ids=None):
+        return self.pl.predict(X, task_id, transformer_ids=transformer_ids)
 
-    def predict_proba(self, X, task_id):
-        return self.pl.predict_proba(X, task_id)
+    def predict_proba(self, X, task_id, transformer_ids=None):
+        return self.pl.predict_proba(X, task_id, transformer_ids=transformer_ids)
