eb6c01fe5bf22b099ce13ee5131e2957d8148730
latasianguy <54046695+latasianguy@users.noreply.github.com>
2020-09-14 22:46:32 -0400

    Create test_lifelongclassificationforest.py
    Issue #100 , Sprint 0.
    
    Add unit tests for LifelongClassificationForest class.
    Implemented methods to check the default initialization attributes and test random parameters.

---
+++ tests/test_lifelongclassificationforest.py
@@ -0,0 +1,52 @@
+import unittest
+import pytest
+import numpy as np
+import random
+
+from proglearn.forest import LifelongClassificationForest
+from proglearn.transformers import TreeClassificationTransformer
+from proglearn.voters import TreeClassificationVoter
+from proglearn.deciders import SimpleAverage
+
+class test_LifelongClassificationForest(unittest.TestCase):
+    
+    def setUp(self):
+        self.l2f = LifelongClassificationForest()
+    
+    def test_initialize(self):
+        self.assertTrue(True)
+        
+    def test_correct_default_transformer(self):
+        self.assertIs(self.l2f.pl.default_transformer_class, TreeClassificationTransformer)
+        
+    def test_correct_default_voter(self):
+        self.assertIs(self.l2f.pl.default_voter_class, TreeClassificationVoter)
+        
+    def test_correct_default_decider(self):
+        self.assertIs(self.l2f.pl.default_decider_class, SimpleAverage)
+        
+    def test_correct_default_kwargs_transformer_decider_empty(self):
+        self.assertFalse(self.l2f.pl.default_transformer_kwargs)
+        self.assertFalse(self.l2f.pl.default_decider_kwargs)
+        
+    def test_correct_default_estimators(self):
+        self.assertIs(self.l2f.n_estimators, 100)
+        
+    def test_correct_estimator(self):
+        rand = random.randint(0, 100)
+        l2f = LifelongClassificationForest(n_estimators=rand)
+        self.assertIs(l2f.n_estimators, rand)
+        
+    def test_correct_default_finite_sample_correction(self):
+        tmpDict = {"finite_sample_correction": False}
+        self.assertEqual(self.l2f.pl.default_voter_kwargs, tmpDict)
+    
+    def test_correct_finite_sample_correction(self):
+        rand = random.randint(0, 1)
+        tmpDict = {"finite_sample_correction": bool(rand)}
+        l2f = LifelongClassificationForest(finite_sample_correction=tmpDict)
+        self.assertEqual(self.l2f.pl.default_voter_kwargs["finite_sample_correction"], bool(rand))
+    
+        
+if __name__ == '__main__':
+    unittest.main()
