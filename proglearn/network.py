96c12ffeff6049890f71cb28f878ad4b57339e34
RebeccaYin7 <65840176+RebeccaYin7@users.noreply.github.com>
2020-09-15 14:14:24 -0400

    Document LifelongClassificationNetwork
    added docstrings to LifelongClassificationNetwork class in network.py

--- proglearn/network.py
+++ proglearn/network.py
@@ -14,6 +14,25 @@ from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 
 class LifelongClassificationNetwork:
+    """
+    A class for progressive learning using Lifelong Learning Networks in a classification setting. 
+    
+    Parameters
+    ----------
+    network: NeuralClassificationTransformer object
+        Transformer network used to map input to output. 
+    loss: string
+        String name of the function used to calculate the loss between labels and predictions. 
+    optimizer: Optimizer object
+        Algorithm used as the optimizer.
+    epochs: int
+        Number of times the entire training set is iterated over. 
+    batch_size: int
+        Number of samples iterated through before making a prediction and generating error. 
+    verbose: bool
+        Boolean indicating the production of detailed logging information. 
+    """
+    
     def __init__(
         self,
         network,
@@ -54,6 +73,21 @@ class LifelongClassificationNetwork:
         )
 
     def add_task(self, X, y, task_id=None, transformer_voter_decider_split=[0.67, 0.33, 0]):
+        """
+        Add a new task to the progressive learner. 
+        
+        Parameters
+        ----------
+        X: ndarray
+            Input data matrix.
+        y: ndarray
+            Output (response) data matrix. 
+        task_id: obj
+            The id corresponding to the task being added. 
+        transformer_voter_decider_split: ndarray
+            Array corresponding to the proportions of data used to train the transformer(s) corresponding to 
+            the task_id, to train the voter(s) from the transformer(s) to the task_id, and to train the decider, respectively.
+        """
         self.pl.add_task(
             X,
             y,
@@ -65,6 +99,19 @@ class LifelongClassificationNetwork:
         return self
     
     def add_transformer(self, X, y, transformer_id=None):
+        """ 
+        Add a new transformer corresponding to the transformer_id. 
+        
+        Parameters
+        ----------
+        X: ndarray
+            Input data matrix.
+        y: ndarray
+            Output (response) data matrix. 
+        transformer_id: obj
+            The transformer you are interested in adding to the progressive learner. 
+        """
+        
         self.pl.add_transformer(
             X,
             y,
@@ -74,7 +121,29 @@ class LifelongClassificationNetwork:
         return self
 
     def predict(self, X, task_id):
+        """
+        Perform inference corresponding to the input task_id using the progressive learner. 
+        
+        Parameters
+        ----------
+        X: ndarray
+            Input data matrix.
+        task_id: obj
+            The task on which you are interested in performing inference. 
+        """
+        
         return self.pl.predict(X, task_id)
 
     def predict_proba(self, X, task_id):
+        """
+        Estimate posteriors under a given task_id using the decider. 
+        
+        Parameters
+        ----------
+        X: ndarray
+            Input data matrix. 
+        task_id: obj
+            The task on which you are interested in estimating posteriors. 
+        """
+        
         return self.pl.predict_proba(X, task_id)
