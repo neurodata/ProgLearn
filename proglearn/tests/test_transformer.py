7adbab461d01df0b85ef329dee56de68e9c7e89d
Will LeVine <levinewill@icloud.com>
2020-09-20 18:48:13 -0400

    Update and rename test_treeclassification.py to test_transformer.py

--- proglearn/tests/test_transformer.py
+++ proglearn/tests/test_transformer.py
@@ -1,13 +1,15 @@
 import pytest
-import unittest
 import numpy as np
 from numpy.testing import assert_allclose
 from sklearn.exceptions import NotFittedError
 
 from proglearn.transformers import TreeClassificationTransformer
-from proglearn.voters import TreeClassificationVoter
 
-class test_treeclassification(unittest.TestCase):
+class test_TreeClassificationTransformer:
+    
+    def test_init(self):
+        TreeClassificationTransformer()
+        assert True
     
     def test_predict_without_fit(self):
         # Generate random data
@@ -15,12 +17,7 @@ class test_treeclassification(unittest.TestCase):
     
         with pytest.raises(NotFittedError):
             trt = TreeClassificationTransformer()
-            trt.transform(X)
-    
-        with pytest.raises(NotFittedError):
-            trv = TreeClassificationVoter()
-            trv.vote(X)
-    
+            trt.transform(X)  
     
     def test_correct_transformation(self):
         np.random.seed(1)
@@ -34,20 +31,3 @@ class test_treeclassification(unittest.TestCase):
         u1 = trt.transform(np.array([0]).reshape(1, -1))
         u2 = trt.transform(np.array([1]).reshape(1, -1))
         assert u1 != u2
-    
-    
-    def test_correct_vote(self):
-        np.random.seed(3)
-    
-        X = np.concatenate((np.zeros(100), np.ones(100)))
-        y = np.concatenate((np.zeros(100), np.ones(100)))
-    
-        trv = TreeClassificationVoter()
-        trv.fit(X, y)
-    
-        X_test = np.ones(6)
-        y_test = np.ones(6)
-        assert_allclose(y_test, trv.vote(X_test)[:,-1], atol=1e-4)
-    
-if __name__ == '__main__':
-    unittest.main()
