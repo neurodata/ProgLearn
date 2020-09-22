f6d47842af37ac41551040f787c1eb5dca5b15ae
latasianguy <54046695+latasianguy@users.noreply.github.com>
2020-09-22 19:28:35 -0400

    Revert "Revert "Merge remote-tracking branch 'upstream/master'""
    This reverts commit 7b7c0b8229cb9e59337b1ebbd4fd0d3d074f2600.

--- proglearn/tests/test_voter.py
+++ proglearn/tests/test_voter.py
@@ -3,31 +3,24 @@ import numpy as np
 
 from sklearn.utils.validation import NotFittedError
 
-from proglearn.progressive_learner import ProgressiveLearner
-from proglearn.deciders import SimpleArgmaxAverage
-from proglearn.transformers import TreeClassificationTransformer, NeuralClassificationTransformer
 from proglearn.voters import TreeClassificationVoter, KNNClassificationVoter
 
-import unittest
-
-from numpy.testing import assert_allclose
+from numpy import testing
 
 def generate_data(n = 100):
     X = np.concatenate([np.zeros(n), np.ones(n)])
     y = np.concatenate([np.zeros(n), np.ones(n)])
     return X, y
 
-class TestTreeClassificationVoter(unittest.TestCase):
+class TestTreeClassificationVoter:
     def test_initialize(self):
         TreeClassificationVoter()
-        self.assertTrue(True)
+        assert True
 
 
     def test_predict_without_fit(self):
         X, y = generate_data()
-        with self.assertRaises(NotFittedError):
-            voter = TreeClassificationVoter()
-            voter.vote(X)
+        testing.assert_raises(NotFittedError, TreeClassificationVoter().vote, X)
 
     def test_correct_predict(self):
         X, y = generate_data()
@@ -37,7 +30,42 @@ class TestTreeClassificationVoter(unittest.TestCase):
 
         y_hat = np.argmax(voter.vote(X), axis = 1)
 
-        assert_allclose(y, y_hat)
+        testing.assert_allclose(y, y_hat)
+        
+class TestKNNClassificationVoter:
+    def test_initialize(self):
+        KNNClassificationVoter()
+        assert True
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_vote_without_fit(self):
+        #generate random data
+        X = np.random.randn(100,3)
+        testing.assert_raises(NotFittedError, KNNClassificationVoter().vote, X)
+        
+    def test_correct_k(self):
+        #generate training data and classes
+        X = np.concatenate((np.zeros(100), np.ones(100))).reshape(-1,1)
+        Y = np.concatenate((np.zeros(100), np.ones(100)))
+
+        #train model
+        assert KNNClassificationVoter(3).fit(X, Y).k == 3
+        assert KNNClassificationVoter().fit(X, Y).k == int(np.log2(len(X)))
+
+    def test_correct_vote(self):
+        #set random seed
+        np.random.seed(0)   
+        
+        #generate training data and classes
+        X = np.concatenate((np.zeros(100), np.ones(100))).reshape(-1,1)
+        Y = np.concatenate((np.zeros(100), np.ones(100)))
+
+        #train model
+        kcv = KNNClassificationVoter(3)
+        kcv.fit(X, Y)
+
+        #generate testing data and class probability
+        X_test = np.ones(6).reshape(-1,1)
+        Y_test = np.concatenate((np.zeros((6,1)), np.ones((6,1))), axis = 1)
+
+        #check if model predicts as expected
+        testing.assert_allclose(Y_test, kcv.vote(X_test), atol=1e-4)
