06ff0a42131505e313332e1b45cf58e7ea6af719
Will LeVine <levinewill@icloud.com>
2020-09-24 19:50:54 -0400

    Added test_network.py

---
+++ proglearn/tests/test_network.py
@@ -0,0 +1,55 @@
+import unittest
+import pytest
+import numpy as np
+import random
+
+from proglearn.forest import LifelongClassificationNetwork
+from proglearn.transformers import NeuralClassificationTransformer
+from proglearn.voters import KNNClassificationVoter
+from proglearn.deciders import SimpleArgmaxAverage
+
+class TestLifelongClassificationNetwork:
+    
+    def test_initialize(self):
+        l2n = LifelongClassificationNetwork()
+        assert True
+        
+    def test_correct_default_transformer(self):
+        l2n = LifelongClassificationNetwork()
+        assert l2n.pl.default_transformer_class == NeuralClassificationTransformer
+        
+    def test_correct_default_voter(self):
+        l2n = LifelongClassificationNetwork()
+        assert l2n.pl.default_voter_class == KNNClassificationVoter
+        
+    def test_correct_default_decider(self):
+        l2n = LifelongClassificationNetwork()
+        assert l2n.pl.default_decider_class == SimpleArgmaxAverage
+        
+    def test_correct_default_kwargs(self):
+        l2n = LifelongClassificationNetwork()
+        
+        #transformer 
+        assert l2f.pl.default_transformer_kwargs == {
+            "network": self.network,
+            "euclidean_layer_idx": -2,
+            "loss": self.loss,
+            "optimizer": self.optimizer,
+            "fit_kwargs": {
+                "epochs": self.epochs,
+                "callbacks": [EarlyStopping(patience=5, monitor="val_loss")],
+                "verbose": self.verbose,
+                "validation_split": 0.33,
+                "batch_size": self.batch_size
+            },
+        }
+        
+        #voter
+        assert l2f.pl.default_voter_kwargs == {}
+        
+        #decider
+        assert l2f.pl.default_decider_kwargs == {}
+        
+    def test_correct_default_transformer_voter_decider_split(self):
+        l2n = LifelongClassificationNetwork()
+        assert l2n.default_transformer_voter_decider_split = [0.67, 0.33, 0]
