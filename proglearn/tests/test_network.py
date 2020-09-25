21700abb454b2bd67abe49740ed98641b6a327e6
Ubuntu <ubuntu@ip-172-31-1-75.us-east-2.compute.internal>
2020-09-25 02:56:46 +0000

    hopefully upped the codecov

---
+++ proglearn/tests/test_network.py
@@ -0,0 +1,45 @@
+import unittest
+import pytest
+import numpy as np
+import random
+
+import keras
+
+from proglearn.network import LifelongClassificationNetwork
+from proglearn.transformers import NeuralClassificationTransformer
+from proglearn.voters import KNNClassificationVoter
+from proglearn.deciders import SimpleArgmaxAverage
+
+class TestLifelongClassificationNetwork:
+    
+    def test_initialize(self):
+        l2n = LifelongClassificationNetwork(keras.Sequential())
+        assert True
+        
+    def test_correct_default_transformer(self):
+        l2n = LifelongClassificationNetwork(keras.Sequential())
+        assert l2n.pl.default_transformer_class == NeuralClassificationTransformer
+        
+    def test_correct_default_voter(self):
+        l2n = LifelongClassificationNetwork(keras.Sequential())
+        assert l2n.pl.default_voter_class == KNNClassificationVoter
+        
+    def test_correct_default_decider(self):
+        l2n = LifelongClassificationNetwork(keras.Sequential())
+        assert l2n.pl.default_decider_class == SimpleArgmaxAverage
+        
+    def test_correct_default_kwargs_length(self):
+        l2n = LifelongClassificationNetwork(keras.Sequential())
+        
+        #transformer 
+        assert len(l2n.pl.default_transformer_kwargs) == 5
+        
+        #voter
+        assert len(l2n.pl.default_voter_kwargs) == 0
+        
+        #decider
+        assert len(l2n.pl.default_decider_kwargs) == 0
+        
+    def test_correct_default_transformer_voter_decider_split(self):
+        l2n = LifelongClassificationNetwork(keras.Sequential())
+        assert l2n.default_transformer_voter_decider_split == [0.67, 0.33, 0]
