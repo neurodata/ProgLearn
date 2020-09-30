c385843c228eae96ba8af73da3119b484ec5e4d8
Will LeVine <levinewill@icloud.com>
2020-09-29 20:34:01 -0400

    Added maintainers to setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,10 @@ with open("README.md", mode="r", encoding = "utf8") as f:
 setup(
     name="proglearn",
     version="0.0.1",
-    author="Will Levine, Jayanta Dey, Hayden Helm",
+    author="Will LeVine, Jayanta Dey, Hayden Helm",
     author_email="levinewill@icloud.com",
+    maintainer="Will LeVine, Jayanta Dey",
+    maintainer_email="levinewill@icloud.com",
     description="A package to implement and extend the methods desribed in 'A General Approach to Progressive Learning'",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
