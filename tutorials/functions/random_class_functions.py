352ee414268d27e1b926d29a0a74810677eaa6d2
Haoyin Xu <hax042@ucsd.edu>
2020-10-06 14:46:04 -0400

    Trim down unnecessary packages

--- tutorials/functions/random_class_functions.py
+++ tutorials/functions/random_class_functions.py
@@ -5,27 +5,24 @@ warnings.simplefilter("ignore")
 
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 
-import time
 import random
-import keras
 
-from keras import layers
 from itertools import product
-from math import log2, ceil
 from joblib import Parallel, delayed
-from multiprocessing import Pool
-from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.model_selection import StratifiedKFold
+
+# Import the packages for plotting
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
+rcParams.update({"figure.autolayout": True})
 
 # Import the progressive learning packages
 from proglearn.progressive_learner import ProgressiveLearner
 from proglearn.deciders import SimpleArgmaxAverage
-from proglearn.transformers import (
-    TreeClassificationTransformer,
-    NeuralClassificationTransformer,
-)
-from proglearn.voters import TreeClassificationVoter, KNNClassificationVoter
+from proglearn.transformers import TreeClassificationTransformer
+from proglearn.voters import TreeClassificationVoter
 
 # The method randomly selects training and testing subsets from the original datasets,
 # making cross-validation on program results possible.
@@ -71,7 +68,7 @@ def cross_val_data(
 
 # The method runs the lifelong learning experiments
 def L2_experiment(
-    data_x, data_y, ntrees, shift, slot, model, num_points_per_task, acorn=None
+    data_x, data_y, ntrees, shift, slot, num_points_per_task, task_num, acorn=None
 ):
 
     # construct dataframes
@@ -79,8 +76,6 @@ def L2_experiment(
     shifts = []
     slots = []
     accuracies_across_tasks = []
-    train_times_across_tasks = []
-    inference_times_across_tasks = []
 
     # randomly separate the training and testing subsets
     train_x_task0, train_y_task0, test_x_task0, test_y_task0 = cross_val_data(
@@ -88,7 +83,6 @@ def L2_experiment(
     )
 
     # choose Uncertainty Forest as transformer
-    # construct the learner with model selected
     progressive_learner = ProgressiveLearner(
         default_transformer_class=TreeClassificationTransformer,
         default_transformer_kwargs={"kwargs": {"max_depth": 30}},
@@ -98,7 +92,6 @@ def L2_experiment(
     )
 
     # training process
-    train_start_time = time.time()
     progressive_learner.add_task(
         X=train_x_task0,
         y=train_y_task0,
@@ -106,22 +99,17 @@ def L2_experiment(
         transformer_voter_decider_split=[0.67, 0.33, 0],
         decider_kwargs={"classes": np.unique(train_y_task0)},
     )
-    train_end_time = time.time()
 
     # testing process
-    inference_start_time = time.time()
     task_0_predictions = progressive_learner.predict(test_x_task0, task_id=0)
-    inference_end_time = time.time()
 
     # record results
     shifts.append(shift)
     slots.append(slot)
     accuracies_across_tasks.append(np.mean(task_0_predictions == test_y_task0))
-    train_times_across_tasks.append(train_end_time - train_start_time)
-    inference_times_across_tasks.append(inference_end_time - inference_start_time)
 
-    # repeating the tasks for 20 times
-    for task_ii in range(1, 20):
+    # repeating the tasks for task_num times
+    for task_ii in range(1, task_num):
 
         # randomly separate the training and testing subsets
         train_x, train_y, _, _ = cross_val_data(
@@ -135,7 +123,6 @@ def L2_experiment(
         )
 
         # training process
-        train_start_time = time.time()
         progressive_learner.add_transformer(
             X=train_x,
             y=train_y,
@@ -143,26 +130,19 @@ def L2_experiment(
             num_transformers=ntrees,
             backward_task_ids=[0],
         )
-        train_end_time = time.time()
 
         # testing process
-        inference_start_time = time.time()
         task_0_predictions = progressive_learner.predict(test_x_task0, task_id=0)
-        inference_end_time = time.time()
 
         # record results
         shifts.append(shift)
         slots.append(slot)
         accuracies_across_tasks.append(np.mean(task_0_predictions == test_y_task0))
-        train_times_across_tasks.append(train_end_time - train_start_time)
-        inference_times_across_tasks.append(inference_end_time - inference_start_time)
 
     # finalize dataframes
     df["data_fold"] = shifts
     df["slot"] = slots
     df["accuracy"] = accuracies_across_tasks
-    df["train_times"] = train_times_across_tasks
-    df["inference_times"] = inference_times_across_tasks
 
     # save results
     return df
@@ -170,7 +150,7 @@ def L2_experiment(
 
 # The method allows multiple lifelong forest experiments to be run at the same time
 def run_parallel_exp(
-    data_x, data_y, n_trees, model, num_points_per_task, slot=0, shift=1
+    data_x, data_y, n_trees, num_points_per_task, task_num, slot=0, shift=1
 ):
     return L2_experiment(
         data_x,
@@ -178,8 +158,8 @@ def run_parallel_exp(
         n_trees,
         shift,
         slot,
-        model,
         num_points_per_task,
+        task_num,
         acorn=12345,
     )
 
@@ -187,8 +167,6 @@ def run_parallel_exp(
 # The method calculates the bte and time results
 def calculate_results(df_results, slot_num, shift_num):
     btes = []
-    train_times = []
-    inference_times = []
 
     # read the experiment results
     for slot in range(slot_num):
@@ -203,20 +181,11 @@ def calculate_results(df_results, slot_num, shift_num):
 
             # record information
             btes.append(bte)
-            train_times.append(df["train_times"])
-            inference_times.append(df["inference_times"])
 
-    return btes, train_times, inference_times
-
-
-# Import the packages for plotting
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
-
-rcParams.update({"figure.autolayout": True})
+    return btes
 
 # The method plots the Backward Transfer Efficiency
-def plot_bte(bte):
+def plot_bte(bte, fontsize, ticksize):
 
     # plot the BTE
     ax = plt.subplot(111)
@@ -242,47 +211,8 @@ def plot_bte(bte):
     ax.set_ylabel("BTE (Task 1)", fontsize=fontsize)
     ax.set_yticks([1.0, 1.1, 1.2])
     ax.tick_params(labelsize=ticksize)
-    ax.legend(fontsize=22)
+    ax.legend(fontsize=fontsize)
 
     # show the BTE plot
-    plt.tight_layout()
-    plt.show()
-
-
-# The method plots the time changes
-def plot_time(train_time_across_tasks, inference_time_across_tasks):
-
-    # plot the recorded times
-    ax = plt.subplot(111)
-
-    # Hide the right and top spines
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
-
-    # Only show ticks on the left and bottom spines
-    ax.yaxis.set_ticks_position("left")
-    ax.xaxis.set_ticks_position("bottom")
-
-    ax.plot(
-        range(len(train_time_across_tasks)),
-        train_time_across_tasks,
-        linewidth=3,
-        linestyle="solid",
-        label="Train Time",
-    )
-    ax.plot(
-        range(len(inference_time_across_tasks)),
-        inference_time_across_tasks,
-        linewidth=3,
-        linestyle="solid",
-        label="Inference Time",
-    )
-
-    ax.set_xlabel("Number of Tasks Seen", fontsize=fontsize)
-    ax.set_ylabel("Time (seconds)", fontsize=fontsize)
-    ax.tick_params(labelsize=ticksize)
-    ax.legend(fontsize=22)
-
-    # show the time plot
     plt.tight_layout()
     plt.show()
