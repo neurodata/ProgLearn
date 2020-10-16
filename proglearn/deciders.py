6d30481462d7a1d2c6ddc8e7edffc3e27ebbadc4
Will LeVine <levinewill@icloud.com>
2020-10-16 13:05:20 -0400

    Update deciders.py

--- proglearn/deciders.py
+++ proglearn/deciders.py
@@ -9,60 +9,35 @@ from .base import BaseClassificationDecider
 from sklearn.utils.validation import (
     check_X_y,
     check_array,
-    NotFittedError,
+    check_is_fitted,
 )
-
-from sklearn.utils.multiclass import type_of_target
-
 
 class SimpleArgmaxAverage(BaseClassificationDecider):
     """
     A class for a decider that uses the average vote for classification.
     Uses BaseClassificationDecider as a base class.
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     classes : list, default=[]
         List of final output classification labels of type obj.
         Defaults to an empty list of classes.
 
-    Attributes (objects):
-    -----------
-    classes : list, default=[]
-        List of final output classification labels of type obj.
-        Defaults to an empty list of classes.
-
-    _is_fitted : boolean, default=False
-        Boolean variable to see if the decider is fitted, defaults to False
-
-    transformer_id_to_transformers : dict
+    Attributes
+    ----------
+    transformer_id_to_transformers_ : dict
         A dictionary with keys of type obj corresponding to transformer ids
         and values of type obj corresponding to a transformer. This dictionary
         maps transformers to a particular transformer id.
 
-    transformer_id_to_voters : dict
+    transformer_id_to_voters_ : dict
         A dictionary with keys of type obj corresponding to transformer ids
         and values of type obj corresponding to a voter class. This dictionary
         maps voter classes to a particular transformer id.
-
-    Methods
-    -----------
-    fit(X, y, transformer_id_to_transformers, transformer_id_to_voters, classes=None)
-        Fits the decider to inputs X and final classification outputs y.
-
-    predict_proba(X, transformers_id=None)
-        Predicts posterior probabilities given input data, X, for each class.
-
-    predict(X, transformer, transformer_ids=None)
-        Predicts the most likely class given input data X.
-
-    is_fitted()
-        Returns if the decider has been fitted.
     """
 
     def __init__(self, classes=[]):
         self.classes = classes
-        self._is_fitted = False
 
     def fit(
         self,
@@ -70,7 +45,6 @@ class SimpleArgmaxAverage(BaseClassificationDecider):
         y,
         transformer_id_to_transformers,
         transformer_id_to_voters,
-        classes=None,
     ):
         """
         Function for fitting.
@@ -95,9 +69,6 @@ class SimpleArgmaxAverage(BaseClassificationDecider):
             and values of type obj corresponding to a voter class. This dictionary thus
             maps voter classes to a particular transformer id.
 
-        classes : list, default=None
-            List of final output classification labels of type obj.
-
         Raises:
         -----------
         ValueError :
@@ -117,10 +88,8 @@ class SimpleArgmaxAverage(BaseClassificationDecider):
                 self.classes = np.unique(y)
         else:
             self.classes = np.array(self.classes)
-        self.transformer_id_to_transformers = transformer_id_to_transformers
-        self.transformer_id_to_voters = transformer_id_to_voters
-
-        self._is_fitted = True
+        self.transformer_id_to_transformers_ = transformer_id_to_transformers_
+        self.transformer_id_to_voters_ = transformer_id_to_voters_
         return self
 
     def predict_proba(self, X, transformer_ids=None):
@@ -143,36 +112,31 @@ class SimpleArgmaxAverage(BaseClassificationDecider):
             to using all transformers if no transformer ids are given.
 
         Raises:
-        -----------
+        -------
         NotFittedError :
             When the model is not fitted.
 
         Returns:
-        -----------
+        --------
         Returns mean vote across transformer ids as an ndarray.
         """
+        check_is_fitted(self)
         vote_per_transformer_id = []
         for transformer_id in (
             transformer_ids
             if transformer_ids is not None
-            else self.transformer_id_to_voters.keys()
+            else self.transformer_id_to_voters_.keys()
         ):
-            if not self.is_fitted():
-                msg = (
-                    "This %(name)s instance is not fitted yet. Call 'fit' with "
-                    "appropriate arguments before using this decider."
-                )
-                raise NotFittedError(msg % {"name": type(self).__name__})
-
+            check_is_fitted(self)
             vote_per_bag_id = []
             for bag_id in range(
-                len(self.transformer_id_to_transformers[transformer_id])
+                len(self.transformer_id_to_transformers_[transformer_id])
             ):
-                transformer = self.transformer_id_to_transformers[transformer_id][
+                transformer = self.transformer_id_to_transformers_[transformer_id][
                     bag_id
                 ]
                 X_transformed = transformer.transform(X)
-                voter = self.transformer_id_to_voters[transformer_id][bag_id]
+                voter = self.transformer_id_to_voters_[transformer_id][bag_id]
                 vote = voter.predict_proba(X_transformed)
                 vote_per_bag_id.append(vote)
             vote_per_transformer_id.append(np.mean(vote_per_bag_id, axis=0))
@@ -193,27 +157,15 @@ class SimpleArgmaxAverage(BaseClassificationDecider):
         transformer_ids : list, default=None
             A list with all transformer ids. Defaults to None if no transformer ids
             are given.
+            
+        Raises:
+        -------
+        NotFittedError :
+            When the model is not fitted.
 
         Returns:
-        -----------
+        --------
         The class with the highest vote based on the argmax of the votes as an int.
         """
-        if not self.is_fitted():
-            msg = (
-                "This %(name)s instance is not fitted yet. Call 'fit' with "
-                "appropriate arguments before using this decider."
-            )
-            raise NotFittedError(msg % {"name": type(self).__name__})
-
         vote_overall = self.predict_proba(X, transformer_ids=transformer_ids)
         return self.classes[np.argmax(vote_overall, axis=1)]
-
-    def is_fitted(self):
-        """
-        Getter function to check if the decider is fitted.
-
-        Returns:
-        -----------
-        Boolean class attribute _is_fitted.
-        """
-        return self._is_fitted
