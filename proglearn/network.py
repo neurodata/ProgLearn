c64179af0c3ca90a4cc44e83ba115d9dd175d8b6
Will LeVine <levinewill@icloud.com>
2020-10-19 12:06:57 -0400

    Fixed network.py default overload

--- proglearn/network.py
+++ proglearn/network.py
@@ -95,7 +95,7 @@ class LifelongClassificationNetwork(ClassificationProgressiveLearner):
             default_decider_kwargs={},
         )
 
-    def add_task(self, X, y, task_id=None, transformer_voter_decider_split=None):
+    def add_task(self, X, y, task_id=None, transformer_voter_decider_split='default'):
         """
         adds a task with id task_id, given input data matrix X
         and output data matrix y, to the Lifelong Classification Network
@@ -111,18 +111,18 @@ class LifelongClassificationNetwork(ClassificationProgressiveLearner):
         task_id: obj
             The id corresponding to the task being added.
 
-        transformer_voter_decider_split: ndarray, default=None
+        transformer_voter_decider_split: ndarray, default='default'
             1D array of length 3 corresponding to the proportions of data used to train the
             transformer(s) corresponding to the task_id, to train the voter(s) from the
             transformer(s) to the task_id, and to train the decider for task_id, respectively.
-            The default is used if 'None' is provided.
+            The default is used if 'default' is provided.
 
         Returns
         -------
         self : LifelongClassificationNetwork
             The object itself.
         """
-        if transformer_voter_decider_split is None:
+        if transformer_voter_decider_split == 'default':
             transformer_voter_decider_split = (
                 self.default_transformer_voter_decider_split
             )
