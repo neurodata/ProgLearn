48d5d21ea41b728b94153878f421377ead6f543f
Michael Ainsworth <michael.j.ainsworth@gmail.com>
2020-10-20 14:07:58 -0400

    Add docstrings to model functions

--- docs/tutorials/sample_size_experiment.py
+++ docs/tutorials/sample_size_experiment.py
@@ -1,5 +1,4 @@
 from sklearn.ensemble import RandomForestClassifier
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
@@ -17,6 +16,23 @@ __all__ = [
 
 
 def random_forest_classifier_model(X_train, y_train, num_trees, max_depth, verbose):
+    """
+    Returns a random forest classifier model trained on data X_train and labels y_train
+
+    Args:
+        X_train : ndarray
+            A numpy array of input data with shape [num_examples, num_features]
+        y_train : ndarray
+            A numpy array of data labels with shape [num_examples,]
+        num_trees : int
+            An int to identify number of trees to train the classifier with
+        max_depth : int
+            An int to identify the max depth of each tree in the classifier
+
+    Returns:
+        rf_model : object
+            A random forest classifier model for predicting new data
+    """
 
     rf_model = RandomForestClassifier(
         n_estimators=num_trees, max_depth=max_depth, verbose=verbose
@@ -29,6 +45,29 @@ def random_forest_classifier_model(X_train, y_train, num_trees, max_depth, verbo
 def binary_deep_neural_network(
     X_train, y_train, epochs, batch_size, learning_rate, validation_split, verbose
 ):
+    """
+    Returns a binary neural network model trained on data X_train and labels y_train
+
+    Args:
+        X_train : ndarray
+            A numpy array of input data with shape [num_examples, num_features]
+        y_train : ndarray
+            A numpy array of data labels with shape [num_examples,]
+        epochs : int
+            An int to identify the total number of epochs the models is trained with
+        batch_size : int
+            An int to identify the batch size for training the model
+        learning_rate : float
+            A float to identify the learning rate of the Adam optimizer
+        validation_split : float
+            A float between 0 and 1 to represent the amount of training data to be used for validation
+        verbose : int
+            An int to represent the level of output while training the model
+
+    Returns:
+        dnn_model : object
+            A trained binary neural network
+    """
 
     dnn_model = Sequential()
 
@@ -63,6 +102,31 @@ def multiclass_deep_neural_network(
     validation_split,
     verbose,
 ):
+    """
+    Returns a multiclass neural network model trained on data X_train and labels y_train
+
+    Args:
+        X_train : ndarray
+            A numpy array of input data with shape [num_examples, num_features]
+        y_train : ndarray
+            A numpy array of data labels with shape [num_examples,]
+        output_nodes : int
+            An int corresponding to the number of classes in the dataset if greater than 2
+        epochs : int
+            An int to identify the total number of epochs the models is trained with
+        batch_size : int
+            An int to identify the batch size for training the model
+        learning_rate : float
+            A float to identify the learning rate of the Adam optimizer
+        validation_split : float
+            A float between 0 and 1 to represent the amount of training data to be used for validation
+        verbose : int
+            An int to represent the level of output while training the model
+
+    Returns:
+        dnn_model_mc : object
+            A trained multiclass neural network
+    """
 
     dnn_model_mc = Sequential()
 
@@ -97,6 +161,32 @@ def parametric_convolutional_neural_network(
     validation_split,
     verbose,
 ):
+    """
+    Returns a convolutional neural network model trained on data X_train and labels y_train
+
+    Args:
+        X_train : ndarray
+            A numpy array of input data
+        y_train : ndarray
+            A numpy array of data labels with shape [num_examples,]
+        complexity : int
+            An int to represent an arbitrary complexity value where each additional int over
+            2 adds another convolutional layer to the model
+        epochs : int
+            An int to identify the total number of epochs the models is trained with
+        batch_size : int
+            An int to identify the batch size for training the model
+        learning_rate : float
+            A float to identify the learning rate of the Adam optimizer
+        validation_split : float
+            A float between 0 and 1 to represent the amount of training data to be used for validation
+        verbose : int
+            An int to represent the level of output while training the model
+
+    Returns:
+        cnn_model : object
+            A trained convolutional neural network
+    """
 
     cnn_model = Sequential()
 
