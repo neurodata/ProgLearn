65337eb44208292070a6a63097e242381f7b0d80
phillip-teng <phillip@scripthealth.co>
2020-12-20 22:26:27 -0800

    File structure change

--- docs/tutorials/functions/binning.py
+++ docs/tutorials/functions/binning.py
@@ -2,11 +2,14 @@ import numpy as np
 import math
 from sklearn.preprocessing import KBinsDiscretizer
 
-def KBinsDiscretize(data_x, n_bins=0, encode="ordinal", strategy="uniform"):
+def KBinsDiscretize(data_x, n_bins=0, alpha=3.322, encode="ordinal", strategy="uniform"):
+    """
+        
+    """
    # Makes n_bins optional, calculates optimal n_bins by default
    # Sturges Rule - num_bins = 1 + 3.322 * log_10(num_inputs)
    if n_bins == 0:
-      n_bins = math.floor(1 + 3.322 * math.log10(data_x.shape[0], 2))
+      n_bins = math.floor(1 + alpha * math.log10(data_x.shape[0], 2))
       if n_bins > 256:
          n_bins = 256 # cap n_bins at 256
    binner = KBinsDiscretizer(n_bins, encode="ordinal", strategy="uniform")
