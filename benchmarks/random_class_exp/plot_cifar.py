88bf4e3f006e651977b1c42db349afa43b99f89c
Ubuntu <ubuntu@ip-172-31-10-48.ec2.internal>
2021-01-19 18:22:12 +0000

    random class experiment

--- benchmarks/random_class_exp/plot_cifar.py
+++ benchmarks/random_class_exp/plot_cifar.py
@@ -12,8 +12,8 @@ slots = 10
 shifts = 6
 alg_num = 1
 task_num = 9
-model = "uf"
-ntrees = 10
+model = ["uf", "dnn"]
+ntrees = [10, 0]
 ########################
 
 
@@ -27,39 +27,39 @@ def unpickle(file):
 #%%
 reps = slots * shifts
 
-btes = []
-train_times = []
-inference_times = []
+btes = [[], []]
+btes_ = []
 
-for slot in range(0, slots):
-    for shift in range(shifts):
-        filename = (
-            "result/"
-            + model
-            + str(ntrees)
-            + "_"
-            + str(shift + 1)
-            + "_"
-            + str(slot)
-            + ".pickle"
-        )
-        df = unpickle(filename)
+for i, mdl in enumerate(model):
+    for slot in range(0, slots):
+        for shift in range(shifts):
+            filename = (
+                "result/"
+                + mdl
+                + str(ntrees[i])
+                + "_"
+                + str(shift + 1)
+                + "_"
+                + str(slot)
+                + ".pickle"
+            )
+            df = unpickle(filename)
 
-        err = 1 - df["accuracy"]
-        bte = err[0] / err
+            err = 1 - df["accuracy"]
+            bte = err[0] / err
 
-        btes.append(bte)
-        train_times.append(df["train_times"])
-        inference_times.append(df["inference_times"])
+            btes_.append(bte)
+            #train_times.append(df["train_times"])
+            #inference_times.append(df["inference_times"])
 
 
-bte = np.mean(btes, axis=0)
-train_time_across_tasks = np.mean(train_times, axis=0)
-inference_time_across_tasks = np.mean(inference_times, axis=0)
+    btes[i] = np.mean(btes_, axis=0)
+#train_time_across_tasks = np.mean(train_times, axis=0)
+#inference_time_across_tasks = np.mean(inference_times, axis=0)
 
 #%%
-fontsize = 22
-ticksize = 20
+fontsize = 20
+ticksize = 16
 
 ax = plt.subplot(111)
 
@@ -71,27 +71,34 @@ ax.spines["top"].set_visible(False)
 ax.yaxis.set_ticks_position("left")
 ax.xaxis.set_ticks_position("bottom")
 
-# fig.suptitle('ntrees = '+str(ntrees),fontsize=25)
 ax.plot(
-    range(len(bte)),
-    bte,
-    c="blue" if model == "dnn" else "red",
+    range(len(btes[0])),
+    btes[0],
+    c="#e41a1c",
     linewidth=3,
     linestyle="solid",
-    label="L2N" if model == "dnn" else "L2F",
+    label="PLF"
+)
+ax.plot(
+    range(len(btes[1])),
+    btes[1],
+    c="#377eb8",
+    linewidth=3,
+    linestyle="solid",
+    label="PLN"
 )
 
 ax.set_xlabel("Number of Tasks Seen", fontsize=fontsize)
-ax.set_ylabel("Tranvsfer Efficiency (Task 1)", fontsize=fontsize)
+ax.set_ylabel("Transfer Efficiency (Task 1)", fontsize=fontsize)
 ax.set_yticks([1.0, 1.1, 1.2])
 ax.tick_params(labelsize=ticksize)
-ax.legend(fontsize=22)
+ax.legend(fontsize=20, frameon=False)
 
 plt.tight_layout()
 plt.savefig("./result/figs/random_class.pdf", dpi=300)
 plt.close()
 
-ax = plt.subplot(111)
+'''ax = plt.subplot(111)
 
 # Hide the right and top spines
 ax.spines["right"].set_visible(False)
@@ -123,6 +130,6 @@ ax.tick_params(labelsize=ticksize)
 ax.legend(fontsize=22)
 
 plt.tight_layout()
-plt.savefig("./result/figs/random_class_time.pdf", dpi=300)
+plt.savefig("./result/figs/random_class_time.pdf", dpi=300)'''
 
 # %%
