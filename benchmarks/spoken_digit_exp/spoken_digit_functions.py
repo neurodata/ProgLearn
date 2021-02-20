4cbb358070c61f697568311b89d37507e1600087
Chenyu Gao <cgao17@jhu.edu>
2021-02-20 15:05:24 -0500

    debug and clear unnecessary files
    fix the bug in calculate_result function.
    caches are removed.
    only one figure remains.

--- benchmarks/spoken_digit_exp/spoken_digit_functions.py
+++ benchmarks/spoken_digit_exp/spoken_digit_functions.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 import IPython.display as ipd
 import cv2
@@ -9,17 +8,18 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from numpy import save, load
 from keras import layers
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+
 from proglearn.deciders import SimpleArgmaxAverage
 from proglearn.progressive_learner import ProgressiveLearner
 from proglearn.transformers import NeuralClassificationTransformer, TreeClassificationTransformer
 from proglearn.voters import TreeClassificationVoter, KNNClassificationVoter
-from sklearn.model_selection import train_test_split
 
 from tensorflow.keras.backend import clear_session # To avoid OOM error when using dnn
+
 
 def load_spoken_digit(path_recordings):
     file = os.listdir(path_recordings)
@@ -77,18 +77,19 @@ def display_spectrogram(x_spec_mini, y_number, y_speaker, num):
     plt.suptitle("Short-Time Fourier Transform Spectrogram of Number " + str(num), fontsize=18)
 
 
-def single_experiment(x, y, y_speaker, ntrees=20, model='uf', shuffle=False):
+def single_experiment(x, y, y_speaker, ntrees=10, model='uf', shuffle=False):
     num_tasks = 6
     num_points_per_task = 3000 / num_tasks
     speakers = ['g', 'j', 'l', 'n', 't', 'y']
-    accuracies_across_tasks = []
+    single_task_accuracies = np.zeros(num_tasks, dtype=float)
+    accuracies = np.zeros(27, dtype=float)
 
     if model == 'dnn':
         x_all = x
         y_all = y
-        
+
         clear_session() # clear GPU memory before each run, to avoid OOM error
-        
+
         default_transformer_class = NeuralClassificationTransformer
 
         network = keras.Sequential()
@@ -142,102 +143,75 @@ def single_experiment(x, y, y_speaker, ntrees=20, model='uf', shuffle=False):
     else:
         pass
 
-    for j, task0_speaker in enumerate(speakers):
-        progressive_learner = ProgressiveLearner(default_transformer_class=default_transformer_class,
-                                                 default_transformer_kwargs=default_transformer_kwargs,
-                                                 default_voter_class=default_voter_class,
-                                                 default_voter_kwargs=default_voter_kwargs,
-                                                 default_decider_class=default_decider_class)
+    progressive_learner = ProgressiveLearner(default_transformer_class=default_transformer_class,
+                                             default_transformer_kwargs=default_transformer_kwargs,
+                                             default_voter_class=default_voter_class,
+                                             default_voter_kwargs=default_voter_kwargs,
+                                             default_decider_class=default_decider_class)
 
+    train_x_task, test_x_task, train_y_task, test_y_task = [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [],
+                                                                                                                [], [],
+                                                                                                                [],
+                                                                                                                []], [
+                                                               [], [], [], [], [], []]
+
+    for j, task0_speaker in enumerate(speakers):
         index = np.where(y_speaker == task0_speaker)
         x_task0 = x_all[index]
         y_task0 = y_all[index]
-        train_x_task0, test_x_task0, train_y_task0, test_y_task0 = train_test_split(x_task0, y_task0,
-                                                                                    test_size=0.25)
+        train_x_task[j], test_x_task[j], train_y_task[j], test_y_task[j] = train_test_split(x_task0, y_task0,
+                                                                                            test_size=0.45)
+
+        # print(train_y_task[j], train_x_task[j].shape)
         progressive_learner.add_task(
-            X=train_x_task0,
-            y=train_y_task0,
-            task_id=0,
+            X=train_x_task[j],
+            y=train_y_task[j],
+            task_id=j,
             num_transformers=1 if model == "dnn" else ntrees,
             transformer_voter_decider_split=[0.67, 0.33, 0],
-            decider_kwargs={"classes": np.unique(train_y_task0)},
+            decider_kwargs={"classes": np.unique(train_y_task[j])},
         )
-        task_0_predictions = progressive_learner.predict(test_x_task0, task_id=0)
-        accuracies_across_tasks.append(np.mean(task_0_predictions == test_y_task0))
+        uf_predictions = progressive_learner.predict(
+            X=test_x_task[j], transformer_ids=[j], task_id=j
+        )
+        accuracies[j] = np.mean(uf_predictions == test_y_task[j])
 
         for k, contribute_speaker in enumerate(speakers):
-            if k == j:
+            if k > j:
                 pass
             else:
-                index = np.where(y_speaker == contribute_speaker)
-                x_train = x_all[index]
-                y_train = y_all[index]
-                progressive_learner.add_transformer(
-                    X=x_train,
-                    y=y_train,
-                    transformer_data_proportion=1,
-                    num_transformers=1 if model == "dnn" else ntrees,
-                    backward_task_ids=[0],
-                )
-            task_0_predictions = progressive_learner.predict(test_x_task0, task_id=0)
-            accuracies_across_tasks.append(np.mean(task_0_predictions == test_y_task0))
-    
-    current_time = time.strftime("%H%M%S",time.localtime())
-    filename = 'result/cache/accuracies_ntree'+str(ntrees)+'_'+model+current_time+'.npy'
-    save(filename, accuracies_across_tasks)
+                odif_predictions = progressive_learner.predict(test_x_task[k], task_id=k)
 
-    return accuracies_across_tasks
+            accuracies[6 + k + (j * (j + 1)) // 2] = np.mean(odif_predictions == test_y_task[k])
 
-    
-def calculate_results(accuracy_list):
-    """
-    We have 6 tasks, each task generates 7 accuracies (among which 1 is redundant) in order to compute bte,
-    fte at each points, so there are 42 points in total.
+    return accuracies
 
-    This may help:
 
-    Position of Task0-only accuracy
-    '
-    = = - - - - -
-    - = = - - - -
-    - - = = - - -
-    - - - = = - -
-    - - - - = = -
-    - - - - - = =
-    """
-    accuracy_all = np.array([i for i in accuracy_list])
-    accuracy_all = np.average(accuracy_all, axis = 0)
-    
+def calculate_results(accuracy_all):
     num_tasks = 6
-    acc = [[] for _ in range(num_tasks)]
+    err = [[] for _ in range(num_tasks)]
     for i in range(num_tasks):
         for j in range(num_tasks - i):
-            acc[i].append(accuracy_all[7 * i + i + 1 + j])  # "7*i" first of each row, "+(i+1)" skip to the second "="
+            err[i].append(1 - accuracy_all[6 + ((j + i) * (j + i + 1)) // 2 + i])
 
     bte = [[] for _ in range(num_tasks)]
     for i in range(num_tasks):
         for j in range(num_tasks - i):
-            err_up_to_taskt = 1 - accuracy_all[7 * i + i + 1]
-            err_all_seen = 1 - accuracy_all[7 * i + i + 1 + j]  # "7*i + i+1" is at the second "="
-            bte[i].append(err_up_to_taskt / err_all_seen)
+            bte[i].append(err[i][0] / err[i][j])
 
-    fte = [[] for _ in range(num_tasks)]
+    fte = np.zeros(6, dtype=float)
     for i in range(num_tasks):
-        err_taskt_only = 1 - accuracy_all[7 * i]
-        err_up_to_taskt = 1 - accuracy_all[7 * i + i + 1]
-        fte[i].append(err_taskt_only / err_up_to_taskt)
+        fte[i] = (1 - accuracy_all[i]) / err[i][0]
 
     te = [[] for _ in range(num_tasks)]
     for i in range(num_tasks):
         for j in range(num_tasks - i):
-            err_taskt_only = 1 - accuracy_all[7 * i]
-            err_all_seen = 1 - accuracy_all[7 * i + i + 1 + j]
-            te[i].append(err_taskt_only / err_all_seen)
+            te[i].append((1 - accuracy_all[i]) / err[i][j])
 
-    return acc, bte, fte, te
+    return err, bte, fte, te
 
 
-def plot_results(acc, bte, fte, te, filename):
+def plot_results(acc, bte, fte, te):
     num_tasks = 6
     sns.set()
     clr = ["#e41a1c", "#a65628", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#CCCC00"]
@@ -273,12 +247,72 @@ def plot_results(acc, bte, fte, te, filename):
     ax[1][0].hlines(1, 1, num_tasks, colors='grey', linestyles='dashed', linewidth=1.5)
 
     for i in range(num_tasks):
-        et = np.asarray(acc[i])
+        et = 1 - np.asarray(acc[i])
         ns = np.arange(i + 1, num_tasks + 1)
         ax[1][1].plot(ns, et, c='red', linewidth=2.6)
 
     ax[1][1].set_xlabel('Number of tasks seen', fontsize=fontsize)
     ax[1][1].set_ylabel('Accuracy', fontsize=fontsize)
     ax[1][1].tick_params(labelsize=ticksize)
-    
-    plt.savefig(filename, dpi=300)
+
+
+def plot_paper_figure(bte, fte):
+    sns.set_context("talk")
+    num_tasks = 6
+
+    clr = ["#e41a1c", "#a65628", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#CCCC00"]
+    fontsize = 22
+    ticksize = 20
+
+    fig, ax = plt.subplots(1, 2, figsize=(16, 8))
+    ax[0].plot(np.arange(1, num_tasks + 1), fte, c='red', marker='.', markersize=14, linewidth=3)
+    ax[0].hlines(1, 1, num_tasks, colors='grey', linestyles='dashed', linewidth=1.5)
+    ax[0].tick_params(labelsize=ticksize)
+    ax[0].set_xlabel('Number of tasks seen', fontsize=fontsize)
+    ax[0].set_ylabel('log Forward TE', fontsize=fontsize)
+    ax[0].set_yticks([1, 1.25, 1.5])
+
+    log_lbl = np.round(
+        np.log([1, 1.25, 1.5]),
+        2
+    )
+    labels = [item.get_text() for item in ax[0].get_yticklabels()]
+
+    for ii, _ in enumerate(labels):
+        labels[ii] = str(log_lbl[ii])
+
+    ax[0].set_yticklabels(labels)
+
+    right_side = ax[0].spines["right"]
+    right_side.set_visible(False)
+    top_side = ax[0].spines["top"]
+    top_side.set_visible(False)
+
+    for i in range(num_tasks):
+        et = np.asarray(bte[i])
+        ns = np.arange(i + 1, num_tasks + 1)
+        ax[1].plot(ns, et, c='red', linewidth=2.6)
+
+    ax[1].set_xlabel('Number of tasks seen', fontsize=fontsize)
+    ax[1].set_ylabel('log Backward TE', fontsize=fontsize)
+    ax[1].tick_params(labelsize=ticksize)
+    ax[1].hlines(1, 1, num_tasks, colors='grey', linestyles='dashed', linewidth=1.5)
+    ax[1].set_yticks([1, 1.35, 1.7])
+
+    log_lbl = np.round(
+        np.log([1, 1.35, 1.7]),
+        2
+    )
+    labels = [item.get_text() for item in ax[1].get_yticklabels()]
+
+    for ii, _ in enumerate(labels):
+        labels[ii] = str(log_lbl[ii])
+
+    ax[1].set_yticklabels(labels)
+
+    right_side = ax[1].spines["right"]
+    right_side.set_visible(False)
+    top_side = ax[1].spines["top"]
+    top_side.set_visible(False)
+
+    plt.savefig('language.pdf')
