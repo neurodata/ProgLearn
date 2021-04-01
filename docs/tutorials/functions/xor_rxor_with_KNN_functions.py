150bfbca5277d4aec19c2e7d0eb8261ba47956b3
latasianguy <54046695+latasianguy@users.noreply.github.com>
2021-04-01 11:50:54 -0400

    update with some trial experiments

--- docs/tutorials/functions/xor_rxor_with_KNN_functions.py
+++ docs/tutorials/functions/xor_rxor_with_KNN_functions.py
@@ -134,7 +134,7 @@ def experiment(n_task1, n_task2, n_test=1000,
     default_decider_class = SimpleArgmaxAverage
     default_decider_kwargs = {"classes" : np.arange(2)}
     
-    KNN_class = KNNSimpleClassificationDecider
+    KNN_class = KNNSimpleClassificationDecider #HERE
     KNN_decider_kwargs = {"classes" : np.arange(2), "k" : 1}
     
     progressive_learner = ProgressiveLearner(default_transformer_class = default_transformer_class,
@@ -197,7 +197,175 @@ def experiment(n_task1, n_task2, n_test=1000,
         errors[5] = 0.5
     else:
         progressive_learner.add_task(X_task1, y_task1, num_transformers=n_trees, transformer_voter_decider_split=[0.33, 0.33, 0.33])
-        progressive_learner.add_transformer(X_task2, y_task2, num_transformers=n_trees, transformer_voter_decider_split=[0.33, 0.33, 0.33])
+        progressive_learner.add_task(X_task2, y_task2, num_transformers=n_trees, transformer_voter_decider_split=[0.33, 0.33, 0.33])
+
+        uf.add_task(X_task1, y_task1, num_transformers=2*n_trees)
+        uf.add_task(X_task2, y_task2, num_transformers=2*n_trees)
+        
+        naive_uf_train_x = np.concatenate((X_task1,X_task2),axis=0)
+        naive_uf_train_y = np.concatenate((y_task1,y_task2),axis=0)
+        naive_uf.add_task(
+                naive_uf_train_x, naive_uf_train_y, num_transformers=n_trees
+                )
+        
+        uf_task1=uf.predict(test_task1, transformer_ids=[0], task_id=0)
+        l2f_task1=progressive_learner.predict(test_task1, task_id=0)
+        uf_task2=uf.predict(test_task2, transformer_ids=[1], task_id=1)
+        l2f_task2=progressive_learner.predict(test_task2, task_id=1)
+        naive_uf_task1 = naive_uf.predict(
+            test_task1, transformer_ids=[0], task_id=0
+        )
+        naive_uf_task2 = naive_uf.predict(
+            test_task2, transformer_ids=[0], task_id=0
+        )
+
+        errors[0] = 1 - np.mean(
+            uf_task1 == test_label_task1
+        )
+        errors[1] = 1 - np.mean(
+            l2f_task1 == test_label_task1
+        )
+        errors[2] = 1 - np.mean(
+            uf_task2 == test_label_task2
+        )
+        errors[3] = 1 - np.mean(
+            l2f_task2 == test_label_task2
+        )
+        errors[4] = 1 - np.mean(
+            naive_uf_task1 == test_label_task1
+        )
+        errors[5] = 1 - np.mean(
+            naive_uf_task2 == test_label_task2
+        )
+
+    return errors
+
+def experiment2(n_task1, n_task2, n_test=1000, 
+               task1_angle=0, task2_angle=np.pi/2, 
+               n_trees=10, max_depth=None, random_state=None):
+    
+    """
+    A function to do progressive experiment between two tasks
+    where the task data is generated using Gaussian parity.
+    
+    Parameters
+    ----------
+    n_task1 : int
+        Total number of train sample for task 1.
+    
+    n_task2 : int
+        Total number of train dsample for task 2
+
+    n_test : int, optional (default=1000)
+        Number of test sample for each task.
+        
+    task1_angle : float, optional (default=0)
+        Angle in radian for task 1.
+            
+    task2_angle : float, optional (default=numpy.pi/2)
+        Angle in radian for task 2.
+            
+    n_trees : int, optional (default=10)
+        Number of total trees to train for each task.
+
+    max_depth : int, optional (default=None)
+        Maximum allowable depth for each tree.
+        
+    random_state : int, RandomState instance, default=None
+        Determines random number generation for dataset creation. Pass an int
+        for reproducible output across multiple function calls.
+        
+    
+    Returns
+    -------
+    errors : array of shape [6]
+        Elements of the array is organized as single task error task1,
+        multitask error task1, single task error task2,
+        multitask error task2, naive UF error task1,
+        naive UF task2.
+    """
+
+    if n_task1==0 and n_task2==0:
+        raise ValueError('Wake up and provide samples to train!!!')
+
+    if random_state != None:
+        np.random.seed(random_state)
+
+    errors = np.zeros(6,dtype=float)
+
+    default_transformer_class = TreeClassificationTransformer
+    default_transformer_kwargs = {"kwargs" : {"max_depth" : max_depth}}
+
+    default_voter_class = TreeClassificationVoter
+    default_voter_kwargs = {}
+
+    default_decider_class = SimpleArgmaxAverage
+    default_decider_kwargs = {"classes" : np.arange(2)}
+    
+    KNN_class = KNNClassificationDecider #HERE
+    KNN_decider_kwargs = {"classes" : np.arange(2), "k" : 1}
+    
+    progressive_learner = ProgressiveLearner(default_transformer_class = default_transformer_class,
+                                            default_transformer_kwargs = default_transformer_kwargs,
+                                            default_voter_class = default_voter_class,
+                                            default_voter_kwargs = default_voter_kwargs,
+                                            default_decider_class = KNN_class,
+                                            default_decider_kwargs = KNN_decider_kwargs)
+    uf = ProgressiveLearner(default_transformer_class = default_transformer_class,
+                                            default_transformer_kwargs = default_transformer_kwargs,
+                                            default_voter_class = default_voter_class,
+                                            default_voter_kwargs = default_voter_kwargs,
+                                            default_decider_class = default_decider_class,
+                                            default_decider_kwargs = default_decider_kwargs)
+    naive_uf = ProgressiveLearner(default_transformer_class = default_transformer_class,
+                                            default_transformer_kwargs = default_transformer_kwargs,
+                                            default_voter_class = default_voter_class,
+                                            default_voter_kwargs = default_voter_kwargs,
+                                            default_decider_class = default_decider_class,
+                                            default_decider_kwargs = default_decider_kwargs)
+
+    #source data
+    X_task1, y_task1 = generate_gaussian_parity(n_task1, angle_params=task1_angle)
+    test_task1, test_label_task1 = generate_gaussian_parity(n_test, angle_params=task1_angle)
+
+    #target data
+    X_task2, y_task2 = generate_gaussian_parity(n_task2, angle_params=task2_angle)
+    test_task2, test_label_task2 = generate_gaussian_parity(n_test, angle_params=task2_angle)
+
+    if n_task1 == 0:
+        progressive_learner.add_task(X_task2, y_task2, num_transformers=n_trees, transformer_voter_decider_split=[0.33, 0.33, 0.33])
+
+        errors[0] = 0.5
+        errors[1] = 0.5
+
+        uf_task2=progressive_learner.predict(test_task2,
+                                             transformer_ids=[0], task_id=0)
+        l2f_task2=progressive_learner.predict(test_task2, task_id=0)
+
+        errors[2] = 1 - np.mean(uf_task2 == test_label_task2)
+        errors[3] = 1 - np.mean(l2f_task2 == test_label_task2)
+        
+        errors[4] = 0.5
+        errors[5] = 1 - np.mean(uf_task2 == test_label_task2)
+    elif n_task2 == 0:
+        progressive_learner.add_task(X_task1, y_task1,
+                                     num_transformers=n_trees, transformer_voter_decider_split=[0.33, 0.33, 0.33])
+
+        uf_task1=progressive_learner.predict(test_task1, 
+                                             transformer_ids=[0], task_id=0)
+        l2f_task1=progressive_learner.predict(test_task1, task_id=0)
+
+        errors[0] = 1 - np.mean(uf_task1 == test_label_task1)
+        errors[1] = 1 - np.mean(l2f_task1 == test_label_task1)
+        
+        errors[2] = 0.5
+        errors[3] = 0.5
+        
+        errors[4] = 1 - np.mean(uf_task1 == test_label_task1)
+        errors[5] = 0.5
+    else:
+        progressive_learner.add_task(X_task1, y_task1, num_transformers=n_trees, transformer_voter_decider_split=[0.33, 0.33, 0.33])
+        progressive_learner.add_task(X_task2, y_task2, num_transformers=n_trees, transformer_voter_decider_split=[0.33, 0.33, 0.33])
 
         uf.add_task(X_task1, y_task1, num_transformers=2*n_trees)
         uf.add_task(X_task2, y_task2, num_transformers=2*n_trees)
@@ -503,12 +671,28 @@ def plot_error_and_eff(n1s, n2s, mean_error, mean_te, TASK1, TASK2):
     ax.set_title('Gaussian R-XOR', fontsize=30)
     ax.axis('off')
 
-def bte_v_angle(angle_sweep,task1_sample,task2_sample,mc_rep):
+def bte_v_angle1(angle_sweep,task1_sample,task2_sample,mc_rep):
     mean_te = np.zeros(len(angle_sweep), dtype=float)
     for ii,angle in enumerate(angle_sweep):
         error = np.array(
             Parallel(n_jobs=-1,verbose=0)(
             delayed(experiment)(
+                task1_sample,task2_sample,
+                task2_angle=angle*np.pi/180, 
+                max_depth=ceil(log2(task1_sample))
+            ) for _ in range(mc_rep)
+        )
+        )
+
+        mean_te[ii] = np.mean(error[:,0])/np.mean(error[:,1])
+    return mean_te
+
+def bte_v_angle2(angle_sweep,task1_sample,task2_sample,mc_rep):
+    mean_te = np.zeros(len(angle_sweep), dtype=float)
+    for ii,angle in enumerate(angle_sweep):
+        error = np.array(
+            Parallel(n_jobs=-1,verbose=0)(
+            delayed(experiment2)(
                 task1_sample,task2_sample,
                 task2_angle=angle*np.pi/180, 
                 max_depth=ceil(log2(task1_sample))
