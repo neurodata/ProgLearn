0673046a96b69a28e421e2b66c69cdbc0e946b8d
Rahul Swaminathan <srahul1222@gmail.com>
2021-04-15 11:44:53 -0400

    Run pytest and autoformat function files

--- docs/experiments/functions/fte_bte_dtd_functions.py
+++ docs/experiments/functions/fte_bte_dtd_functions.py
@@ -11,14 +11,17 @@ from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from proglearn.deciders import SimpleArgmaxAverage
 from proglearn.progressive_learner import ProgressiveLearner
-from proglearn.transformers import NeuralClassificationTransformer, TreeClassificationTransformer
+from proglearn.transformers import (
+    NeuralClassificationTransformer,
+    TreeClassificationTransformer,
+)
 from proglearn.voters import TreeClassificationVoter, KNNClassificationVoter
 from sklearn.model_selection import train_test_split
 
 from proglearn.forest import LifelongClassificationForest
 from proglearn.network import LifelongClassificationNetwork
 
-from tensorflow.keras.backend import clear_session # To avoid OOM error when using dnn
+from tensorflow.keras.backend import clear_session  # To avoid OOM error when using dnn
 
 
 def run_fte_bte_exp(data_x, data_y, which_task, model, ntrees=30, shift=0):
@@ -29,8 +32,8 @@ def run_fte_bte_exp(data_x, data_y, which_task, model, ntrees=30, shift=0):
         1
     ):  # Rotates the batch of training samples that are used from each class in each task
         train_x, train_y, test_x, test_y = cross_val_data(data_x, data_y, shift, slot)
-        
-        if model == 'odif':
+
+        if model == "odif":
             # Reshape the data
             train_x = train_x.reshape(
                 train_x.shape[0], train_x.shape[1] * train_x.shape[2] * train_x.shape[3]
@@ -39,29 +42,68 @@ def run_fte_bte_exp(data_x, data_y, which_task, model, ntrees=30, shift=0):
                 test_x.shape[0], test_x.shape[1] * test_x.shape[2] * test_x.shape[3]
             )
 
-        if model == 'odin':     
-            clear_session() # clear GPU memory before each run, to avoid OOM error
+        if model == "odin":
+            clear_session()  # clear GPU memory before each run, to avoid OOM error
 
             default_transformer_class = NeuralClassificationTransformer
 
             network = keras.Sequential()
-            network.add(layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu', input_shape=np.shape(data_x)[1:]))
+            network.add(
+                layers.Conv2D(
+                    filters=16,
+                    kernel_size=(3, 3),
+                    activation="relu",
+                    input_shape=np.shape(data_x)[1:],
+                )
+            )
             network.add(layers.BatchNormalization())
-            network.add(layers.Conv2D(filters=32, kernel_size=(3, 3), strides=2, padding="same", activation='relu'))
+            network.add(
+                layers.Conv2D(
+                    filters=32,
+                    kernel_size=(3, 3),
+                    strides=2,
+                    padding="same",
+                    activation="relu",
+                )
+            )
             network.add(layers.BatchNormalization())
-            network.add(layers.Conv2D(filters=64, kernel_size=(3, 3), strides=2, padding="same", activation='relu'))
+            network.add(
+                layers.Conv2D(
+                    filters=64,
+                    kernel_size=(3, 3),
+                    strides=2,
+                    padding="same",
+                    activation="relu",
+                )
+            )
             network.add(layers.BatchNormalization())
-            network.add(layers.Conv2D(filters=128, kernel_size=(3, 3), strides=2, padding="same", activation='relu'))
+            network.add(
+                layers.Conv2D(
+                    filters=128,
+                    kernel_size=(3, 3),
+                    strides=2,
+                    padding="same",
+                    activation="relu",
+                )
+            )
             network.add(layers.BatchNormalization())
-            network.add(layers.Conv2D(filters=254, kernel_size=(3, 3), strides=2, padding="same", activation='relu'))
+            network.add(
+                layers.Conv2D(
+                    filters=254,
+                    kernel_size=(3, 3),
+                    strides=2,
+                    padding="same",
+                    activation="relu",
+                )
+            )
 
             network.add(layers.Flatten())
             network.add(layers.BatchNormalization())
-            network.add(layers.Dense(2000, activation='relu'))
+            network.add(layers.Dense(2000, activation="relu"))
             network.add(layers.BatchNormalization())
-            network.add(layers.Dense(2000, activation='relu'))
+            network.add(layers.Dense(2000, activation="relu"))
             network.add(layers.BatchNormalization())
-            network.add(layers.Dense(units=4, activation='softmax'))
+            network.add(layers.Dense(units=4, activation="softmax"))
 
             default_transformer_kwargs = {
                 "network": network,
@@ -80,12 +122,14 @@ def run_fte_bte_exp(data_x, data_y, which_task, model, ntrees=30, shift=0):
             default_voter_kwargs = {"k": int(np.log2(360))}
             default_decider_class = SimpleArgmaxAverage
 
-            p_learner = ProgressiveLearner(default_transformer_class=default_transformer_class,
-                                                 default_transformer_kwargs=default_transformer_kwargs,
-                                                 default_voter_class=default_voter_class,
-                                                 default_voter_kwargs=default_voter_kwargs,
-                                                 default_decider_class=default_decider_class)
-        elif model == 'odif':
+            p_learner = ProgressiveLearner(
+                default_transformer_class=default_transformer_class,
+                default_transformer_kwargs=default_transformer_kwargs,
+                default_voter_class=default_voter_class,
+                default_voter_kwargs=default_voter_kwargs,
+                default_decider_class=default_decider_class,
+            )
+        elif model == "odif":
             p_learner = LifelongClassificationForest()
 
         df = fte_bte_experiment(
@@ -185,22 +229,26 @@ def fte_bte_experiment(
 
     for task_num in range((which_task - 1), 10):
         accuracy_per_task = []
-        #print("Starting Task {} For Shift {} For Slot {}".format(task_num, shift, slot))
+        # print("Starting Task {} For Shift {} For Slot {}".format(task_num, shift, slot))
         if acorn is not None:
             np.random.seed(acorn)
 
         # If first task, add task.
         if task_num == (which_task - 1):
-            if model == 'odin':
+            if model == "odin":
                 learner.add_task(
                     X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
                     y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
                     task_id=task_num,
-                    num_transformers = 1,
+                    num_transformers=1,
                     transformer_voter_decider_split=[0.67, 0.33, 0],
-                    decider_kwargs={"classes": np.unique(train_y[(task_num * 360) : ((task_num + 1) * 360)])},
+                    decider_kwargs={
+                        "classes": np.unique(
+                            train_y[(task_num * 360) : ((task_num + 1) * 360)]
+                        )
+                    },
                 )
-            elif model == 'odif':
+            elif model == "odif":
                 learner.add_task(
                     X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
                     y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
@@ -212,43 +260,52 @@ def fte_bte_experiment(
             while t_num < task_num:
                 # Make a prediction on task t using the trained learner on test data
                 llf_task = learner.predict(
-                    test_x[((which_task - 1) * 120) : (which_task * 120), :], task_id=task_num
+                    test_x[((which_task - 1) * 120) : (which_task * 120), :],
+                    task_id=task_num,
                 )
                 acc = np.mean(
                     llf_task == test_y[((which_task - 1) * 120) : (which_task * 120)]
                 )
                 accuracies_across_tasks.append(acc)
-                
-                if model == 'odin':
+
+                if model == "odin":
                     learner.add_task(
                         X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
                         y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
                         task_id=t_num,
-                        num_transformers = 1,
+                        num_transformers=1,
                         transformer_voter_decider_split=[0.67, 0.33, 0],
-                        decider_kwargs={"classes": np.unique(train_y[(task_num * 360) : ((task_num + 1) * 360)])},
+                        decider_kwargs={
+                            "classes": np.unique(
+                                train_y[(task_num * 360) : ((task_num + 1) * 360)]
+                            )
+                        },
                     )
-                elif model == 'odif':
+                elif model == "odif":
                     learner.add_task(
                         X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
                         y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
                         task_id=t_num,
                     )
-                
+
                 # Add task for next task
                 t_num = t_num + 1
 
         else:
-            if model == 'odin':
+            if model == "odin":
                 learner.add_task(
                     X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
                     y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
                     task_id=task_num,
-                    num_transformers = 1,
+                    num_transformers=1,
                     transformer_voter_decider_split=[0.67, 0.33, 0],
-                    decider_kwargs={"classes": np.unique(train_y[(task_num * 360) : ((task_num + 1) * 360)])},
+                    decider_kwargs={
+                        "classes": np.unique(
+                            train_y[(task_num * 360) : ((task_num + 1) * 360)]
+                        )
+                    },
                 )
-            elif model == 'odif':
+            elif model == "odif":
                 learner.add_task(
                     X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
                     y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
@@ -256,12 +313,15 @@ def fte_bte_experiment(
                 )
 
         # Make a prediction on task t using the trained learner on test data
-        predictions = learner.predict(test_x[((which_task - 1) * 120) : (which_task * 120), :], task_id=(which_task - 1))
+        predictions = learner.predict(
+            test_x[((which_task - 1) * 120) : (which_task * 120), :],
+            task_id=(which_task - 1),
+        )
         acc = np.mean(
             predictions == test_y[((which_task - 1) * 120) : (which_task * 120)]
         )
         accuracies_across_tasks.append(acc)
-        #print("Accuracy Across Tasks: {}".format(accuracies_across_tasks))
+        # print("Accuracy Across Tasks: {}".format(accuracies_across_tasks))
 
     df["task"] = range(1, 11)
     df["task_accuracy"] = accuracies_across_tasks
