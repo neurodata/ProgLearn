b2b5b7ca8bf29498ec035863754c2f85fd921bbd
jdey4 <jdey4@jhmi.edu>
2021-08-09 13:52:20 -0500

    exp run with more reps

--- benchmarks/parity_experiment/experiment.py
+++ benchmarks/parity_experiment/experiment.py
@@ -183,7 +183,7 @@ def experiment(n_task1, n_task2, n_test=1000,
 task2_sample_sweep = (2**np.arange(np.log2(60), np.log2(5010)+1, .25)).astype('int')
 task1_sample_ = [1000, 5000]
 task2_angle = 25*np.pi/180
-mc_rep = 10000
+mc_rep = 1000
 
 for task1_sample in task1_sample_:
     mean_te = np.zeros(len(task2_sample_sweep), dtype=float)
