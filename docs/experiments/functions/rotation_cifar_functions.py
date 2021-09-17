rotation cifar fixed

docs/experiments/functions/rotation_cifar_functions.py
@@ -12,7 +12,10 @@ import numpy as np
 import seaborn as sns
 
 # Import the omnidirectional learning packages
-from proglearn.forest import LifelongClassificationForest
+from proglearn.progressive_learner import ProgressiveLearner
+from proglearn.deciders import SimpleArgmaxAverage
+from proglearn.transformers import TreeClassificationTransformer, NeuralClassificationTransformer
+from proglearn.voters import TreeClassificationVoter, KNNClassificationVoter
 
 # Randomized selection of training and testing subsets
 def cross_val_data(data_x, data_y, total_cls=10):
@@ -53,82 +56,66 @@ def cross_val_data(data_x, data_y, total_cls=10):
 
 
 # Runs the experiments
-def odif_experiment(
-    angle, data_x, data_y, granularity, max_depth, reps=1, ntrees=29, acorn=None
-):
-
-    # Set random seed to acorn if acorn is specified
+def odif_experiment(angle, data_x, data_y, reps=1, ntrees=29, acorn=None):
     if acorn is not None:
         np.random.seed(acorn)
 
-    errors = np.zeros(
-        2
-    )  # initializes array of errors that will be generated during each rep
+    errors = np.zeros(2)
 
-    for rep in range(reps):
-        # training and testing subsets are randomly selected by calling the cross_val_data function
-        train_x1, train_y1, train_x2, train_y2, test_x, test_y = cross_val_data(
-            data_x, data_y, total_cls=10
-        )
+    for _ in range(reps):
+        train_x1, train_y1, train_x2, train_y2, test_x, test_y = cross_val_data(data_x, data_y, total_cls=10)
 
-        # Change data angle for second task
+
+        #change data angle for second task
         tmp_data = train_x2.copy()
-        _tmp_ = np.zeros((32, 32, 3), dtype=int)
         total_data = tmp_data.shape[0]
 
         for i in range(total_data):
-            tmp_ = image_aug(tmp_data[i], angle)
-            # 2D image is flattened into a 1D array as random forests can only take in flattened images as inputs
+            tmp_ = image_aug(tmp_data[i],angle)
             tmp_data[i] = tmp_
 
-        # .shape gives the dimensions of each numpy array
-        # .reshape gives a new shape to the numpy array without changing its data
-        train_x1 = train_x1.reshape(
-            (
-                train_x1.shape[0],
-                train_x1.shape[1] * train_x1.shape[2] * train_x1.shape[3],
-            )
+        train_x1 = train_x1.reshape((train_x1.shape[0], train_x1.shape[1] * train_x1.shape[2] * train_x1.shape[3]))
+        tmp_data = tmp_data.reshape((tmp_data.shape[0], tmp_data.shape[1] * tmp_data.shape[2] * tmp_data.shape[3]))
+        test_x = test_x.reshape((test_x.shape[0], test_x.shape[1] * test_x.shape[2] * test_x.shape[3]))
+
+        default_transformer_class = TreeClassificationTransformer
+        default_transformer_kwargs = {"kwargs" : {"max_depth" : 30}}
+
+        default_voter_class = TreeClassificationVoter
+        default_voter_kwargs = {}
+
+        default_decider_class = SimpleArgmaxAverage
+
+
+        progressive_learner = ProgressiveLearner(default_transformer_class = default_transformer_class,
+                                        default_transformer_kwargs = default_transformer_kwargs,
+                                        default_voter_class = default_voter_class,
+                                        default_voter_kwargs = default_voter_kwargs,
+                                        default_decider_class = default_decider_class)
+
+        progressive_learner.add_task(
+            X = train_x1,
+            y = train_y1,
+            transformer_voter_decider_split = [0.67, 0.33, 0],
+            decider_kwargs = {"classes" : np.unique(train_y1)}
         )
-        tmp_data = tmp_data.reshape(
-            (
-                tmp_data.shape[0],
-                tmp_data.shape[1] * tmp_data.shape[2] * tmp_data.shape[3],
-            )
-        )
-        test_x = test_x.reshape(
-            (test_x.shape[0], test_x.shape[1] * test_x.shape[2] * test_x.shape[3])
-        )
-        # number of trees (estimators) to use is passed as an argument because the default is 100 estimators
-        progressive_learner = LifelongClassificationForest(
-             default_max_depth=max_depth
+
+        progressive_learner.add_transformer(
+            X = tmp_data,
+            y = train_y2,
+            transformer_data_proportion = 1,
+            backward_task_ids = [0]
         )
 
-        # Add the original task
-        progressive_learner.add_task(X=train_x1, y=train_y1, n_estimators=ntrees)
+        llf_task1=progressive_learner.predict(test_x, task_id=0)
+        llf_single_task=progressive_learner.predict(test_x, task_id=0, transformer_ids=[0])
 
-        # Predict and get errors for original task
-        llf_single_task = progressive_learner.predict(test_x, task_id=0)
+        errors[1] = errors[1]+(1 - np.mean(llf_task1 == test_y))
+        errors[0] = errors[0]+(1 - np.mean(llf_single_task == test_y))
 
-        # Add the new transformer
-        progressive_learner.add_transformer(X=tmp_data, y=train_y2, n_estimators=ntrees)
-
-        # Predict and get errors with the new transformer
-        llf_task1 = progressive_learner.predict(test_x, task_id=0)
-
-        errors[1] = errors[1] + (
-            1 - np.mean(llf_task1 == test_y)
-        )  # errors from transfer learning
-        errors[0] = errors[0] + (
-            1 - np.mean(llf_single_task == test_y)
-        )  # errors from original task
-
-    errors = (
-        errors / reps
-    )  # errors are averaged across all reps ==> more reps means more accurate errors
-
-    # Average errors for original task and transfer learning are returned for the angle tested
+    errors = errors/reps
     return errors
-
+    
 
 # Rotates the image by the given angle and zooms in to remove unnecessary white space at the corners
 # Some image data is lost during rotation because of the zoom
@@ -164,10 +151,10 @@ def plot_bte(bte, angles):
     # Format and label the plot
     ax.set_xticks([0, 30, 60, 90, 120, 150, 180])
     ax.tick_params(labelsize=20)
-    ax.set_yticks([1, 1.1, 1.15])
+    ax.set_yticks([1, 1.1])
 
     log_lbl = np.round(
-        np.log([1, 1.1, 1.15]),
+        np.log([1, 1.1]),
         2
     )
     labels = [item.get_text() for item in ax.get_yticklabels()]