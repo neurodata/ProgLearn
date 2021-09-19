4dc15b4a6590d9c5206b5f07ddf48d1739e1b684
Haoyin Xu <haoyinxu@gmail.com>
2021-09-18 23:33:05 -0400

    MAINT change names & update import

--- docs/experiments/functions/spoken_digit_functions.py
+++ docs/experiments/functions/spoken_digit_functions.py
@@ -2,23 +2,26 @@ import os
 
 import IPython.display as ipd
 import cv2
-import keras
+from tensorflow import keras
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from keras import layers
-from keras.callbacks import EarlyStopping
-from keras.optimizers import Adam
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 
 from proglearn.deciders import SimpleArgmaxAverage
 from proglearn.progressive_learner import ProgressiveLearner
-from proglearn.transformers import NeuralClassificationTransformer, TreeClassificationTransformer
+from proglearn.transformers import (
+    NeuralClassificationTransformer,
+    TreeClassificationTransformer,
+)
 from proglearn.voters import TreeClassificationVoter, KNNClassificationVoter
 
-from tensorflow.keras.backend import clear_session # To avoid OOM error when using dnn
+from tensorflow.keras.backend import clear_session  # To avoid OOM error
 
 
 def load_spoken_digit(path_recordings):
@@ -33,7 +36,9 @@ def load_spoken_digit(path_recordings):
     for i in file:
         x, sr = librosa.load(path_recordings + i, sr=8000)
         x_stft = librosa.stft(x, n_fft=128)  # Extract STFT
-        x_stft_db = librosa.amplitude_to_db(abs(x_stft))  # Convert an amplitude spectrogram to dB-scaled spectrogram
+        x_stft_db = librosa.amplitude_to_db(
+            abs(x_stft)
+        )  # Convert an amplitude spectrogram to dB-scaled spectrogram
         x_stft_db_mini = cv2.resize(x_stft_db, (28, 28))  # Resize into 28 by 28
         y_n = i[0]  # number
         y_s = i[2]  # first letter of speaker's name
@@ -52,64 +57,126 @@ def load_spoken_digit(path_recordings):
 
 
 def play_audio(num, audio_data, y_number, y_speaker):
-    print('This is number', y_number[num], 'spoken by speaker', y_speaker[num].upper(), '\nDuration:',
-          audio_data[num].shape[0], 'samples in', audio_data[num].shape[0] / 8000, 'seconds')
+    print(
+        "This is number",
+        y_number[num],
+        "spoken by speaker",
+        y_speaker[num].upper(),
+        "\nDuration:",
+        audio_data[num].shape[0],
+        "samples in",
+        audio_data[num].shape[0] / 8000,
+        "seconds",
+    )
     ipd.Audio(audio_data[num], rate=8000)
 
 
 def display_spectrogram(x_spec_mini, y_number, y_speaker, num):
-    label_speaker = ['g', 'j', 'l', 'n', 't', 'y']  # first letter of speaker's name
-    fig, axes = plt.subplots(nrows=6, ncols=8, sharex=True, sharey=True, figsize=(12, 9))
+    label_speaker = ["g", "j", "l", "n", "t", "y"]  # first letter of speaker's name
+    fig, axes = plt.subplots(
+        nrows=6, ncols=8, sharex=True, sharey=True, figsize=(12, 9)
+    )
     for j, speaker in enumerate(label_speaker):
         opt = np.where((y_speaker == speaker) & (y_number == num))[0]
-        ch = np.random.choice(opt, 8, replace=False)  # randomly choose 8 different samples from the speaker to display
+        ch = np.random.choice(
+            opt, 8, replace=False
+        )  # randomly choose 8 different samples from the speaker to display
 
         for i in range(8):
-            axes[j, i].imshow(x_spec_mini[ch[i]], cmap='hot', interpolation='nearest')
-            axes[j, i].axis('off')
+            axes[j, i].imshow(x_spec_mini[ch[i]], cmap="hot", interpolation="nearest")
+            axes[j, i].axis("off")
             if i == 0:
-                axes[j, i].text(-9, 12, 'speaker ' + speaker.upper(), size=14, va='center', rotation='vertical')
+                axes[j, i].text(
+                    -9,
+                    12,
+                    "speaker " + speaker.upper(),
+                    size=14,
+                    va="center",
+                    rotation="vertical",
+                )
             else:
                 continue
 
-    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.05,
-                        wspace=0.05)
-    plt.suptitle("Short-Time Fourier Transform Spectrogram of Number " + str(num), fontsize=18)
+    plt.subplots_adjust(
+        top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.05, wspace=0.05
+    )
+    plt.suptitle(
+        "Short-Time Fourier Transform Spectrogram of Number " + str(num), fontsize=18
+    )
 
 
-def single_experiment(x, y, y_speaker, ntrees=10, model='uf', shuffle=False):
+def single_experiment(x, y, y_speaker, ntrees=10, model="odif", shuffle=False):
     num_tasks = 6
     num_points_per_task = 3000 / num_tasks
-    speakers = ['g', 'j', 'l', 'n', 't', 'y']
+    speakers = ["g", "j", "l", "n", "t", "y"]
     single_task_accuracies = np.zeros(num_tasks, dtype=float)
     accuracies = np.zeros(27, dtype=float)
 
-    if model == 'dnn':
+    if model == "odin":
         x_all = x
         y_all = y
 
-        clear_session() # clear GPU memory before each run, to avoid OOM error
+        clear_session()  # clear GPU memory before each run, to avoid OOM error
 
         default_transformer_class = NeuralClassificationTransformer
 
         network = keras.Sequential()
-        network.add(layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu', input_shape=np.shape(x_all)[1:]))
+        network.add(
+            layers.Conv2D(
+                filters=16,
+                kernel_size=(3, 3),
+                activation="relu",
+                input_shape=np.shape(x_all)[1:],
+            )
+        )
         network.add(layers.BatchNormalization())
-        network.add(layers.Conv2D(filters=32, kernel_size=(3, 3), strides=2, padding="same", activation='relu'))
+        network.add(
+            layers.Conv2D(
+                filters=32,
+                kernel_size=(3, 3),
+                strides=2,
+                padding="same",
+                activation="relu",
+            )
+        )
         network.add(layers.BatchNormalization())
-        network.add(layers.Conv2D(filters=64, kernel_size=(3, 3), strides=2, padding="same", activation='relu'))
+        network.add(
+            layers.Conv2D(
+                filters=64,
+                kernel_size=(3, 3),
+                strides=2,
+                padding="same",
+                activation="relu",
+            )
+        )
         network.add(layers.BatchNormalization())
-        network.add(layers.Conv2D(filters=128, kernel_size=(3, 3), strides=2, padding="same", activation='relu'))
+        network.add(
+            layers.Conv2D(
+                filters=128,
+                kernel_size=(3, 3),
+                strides=2,
+                padding="same",
+                activation="relu",
+            )
+        )
         network.add(layers.BatchNormalization())
-        network.add(layers.Conv2D(filters=254, kernel_size=(3, 3), strides=2, padding="same", activation='relu'))
+        network.add(
+            layers.Conv2D(
+                filters=254,
+                kernel_size=(3, 3),
+                strides=2,
+                padding="same",
+                activation="relu",
+            )
+        )
 
         network.add(layers.Flatten())
         network.add(layers.BatchNormalization())
-        network.add(layers.Dense(2000, activation='relu'))
+        network.add(layers.Dense(2000, activation="relu"))
         network.add(layers.BatchNormalization())
-        network.add(layers.Dense(2000, activation='relu'))
+        network.add(layers.Dense(2000, activation="relu"))
         network.add(layers.BatchNormalization())
-        network.add(layers.Dense(units=10, activation='softmax'))
+        network.add(layers.Dense(units=10, activation="softmax"))
 
         default_transformer_kwargs = {
             "network": network,
@@ -128,7 +195,7 @@ def single_experiment(x, y, y_speaker, ntrees=10, model='uf', shuffle=False):
         default_voter_kwargs = {"k": int(np.log2(num_points_per_task))}
         default_decider_class = SimpleArgmaxAverage
 
-    elif model == 'uf':
+    elif model == "odif":
         x_all = x.reshape(3000, -1)
         y_all = y
 
@@ -143,46 +210,54 @@ def single_experiment(x, y, y_speaker, ntrees=10, model='uf', shuffle=False):
     else:
         pass
 
-    progressive_learner = ProgressiveLearner(default_transformer_class=default_transformer_class,
-                                             default_transformer_kwargs=default_transformer_kwargs,
-                                             default_voter_class=default_voter_class,
-                                             default_voter_kwargs=default_voter_kwargs,
-                                             default_decider_class=default_decider_class)
+    progressive_learner = ProgressiveLearner(
+        default_transformer_class=default_transformer_class,
+        default_transformer_kwargs=default_transformer_kwargs,
+        default_voter_class=default_voter_class,
+        default_voter_kwargs=default_voter_kwargs,
+        default_decider_class=default_decider_class,
+    )
 
-    train_x_task, test_x_task, train_y_task, test_y_task = [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [],
-                                                                                                                [], [],
-                                                                                                                [],
-                                                                                                                []], [
-                                                               [], [], [], [], [], []]
+    train_x_task, test_x_task, train_y_task, test_y_task = (
+        [[], [], [], [], [], []],
+        [[], [], [], [], [], []],
+        [[], [], [], [], [], []],
+        [[], [], [], [], [], []],
+    )
 
     for j, task0_speaker in enumerate(speakers):
         index = np.where(y_speaker == task0_speaker)
         x_task0 = x_all[index]
         y_task0 = y_all[index]
-        train_x_task[j], test_x_task[j], train_y_task[j], test_y_task[j] = train_test_split(x_task0, y_task0,
-                                                                                            test_size=0.45)
+        (
+            train_x_task[j],
+            test_x_task[j],
+            train_y_task[j],
+            test_y_task[j],
+        ) = train_test_split(x_task0, y_task0, test_size=0.45)
 
-        # print(train_y_task[j], train_x_task[j].shape)
         progressive_learner.add_task(
             X=train_x_task[j],
             y=train_y_task[j],
             task_id=j,
-            num_transformers=1 if model == "dnn" else ntrees,
+            num_transformers=1 if model == "odin" else ntrees,
             transformer_voter_decider_split=[0.67, 0.33, 0],
             decider_kwargs={"classes": np.unique(train_y_task[j])},
         )
-        uf_predictions = progressive_learner.predict(
+        odi_predictions = progressive_learner.predict(
             X=test_x_task[j], transformer_ids=[j], task_id=j
         )
-        accuracies[j] = np.mean(uf_predictions == test_y_task[j])
+        accuracies[j] = np.mean(odi_predictions == test_y_task[j])
 
         for k, contribute_speaker in enumerate(speakers):
             if k > j:
                 pass
             else:
-                odif_predictions = progressive_learner.predict(test_x_task[k], task_id=k)
+                odi_predictions = progressive_learner.predict(test_x_task[k], task_id=k)
 
-            accuracies[6 + k + (j * (j + 1)) // 2] = np.mean(odif_predictions == test_y_task[k])
+            accuracies[6 + k + (j * (j + 1)) // 2] = np.mean(
+                odi_predictions == test_y_task[k]
+            )
 
     return accuracies
 
@@ -211,49 +286,62 @@ def calculate_results(accuracy_all):
     return err, bte, fte, te
 
 
-def plot_results(acc, bte, fte, te):
+def plot_results(acc, bte, fte, te, model):
     num_tasks = 6
     sns.set()
-    clr = ["#e41a1c", "#a65628", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#CCCC00"]
+    if model == "odif":
+        clr = "#e41a1c"
+    elif model == "odin":
+        clr = "#377eb8"
     fontsize = 22
     ticksize = 20
 
     fig, ax = plt.subplots(2, 2, figsize=(16, 11.5))
-    ax[0][0].plot(np.arange(1, num_tasks + 1), fte, c='red', marker='.', markersize=14, linewidth=3)
-    ax[0][0].hlines(1, 1, num_tasks, colors='grey', linestyles='dashed', linewidth=1.5)
+    ax[0][0].plot(
+        np.arange(1, num_tasks + 1),
+        fte,
+        c=clr,
+        marker=".",
+        markersize=14,
+        linewidth=3,
+    )
+    ax[0][0].hlines(1, 1, num_tasks, colors="grey", linestyles="dashed", linewidth=1.5)
     ax[0][0].tick_params(labelsize=ticksize)
-    ax[0][0].set_xlabel('Number of tasks seen', fontsize=fontsize)
-    ax[0][0].set_ylabel('FTE', fontsize=fontsize)
+    ax[0][0].set_xlabel("Number of tasks seen", fontsize=fontsize)
+    ax[0][0].set_ylabel("log Forward TE", fontsize=fontsize)
+    ax[0][0].set_yscale("log")
 
     for i in range(num_tasks):
         et = np.asarray(bte[i])
         ns = np.arange(i + 1, num_tasks + 1)
-        ax[0][1].plot(ns, et, c='red', linewidth=2.6)
+        ax[0][1].plot(ns, et, c=clr, linewidth=2.6)
 
-    ax[0][1].set_xlabel('Number of tasks seen', fontsize=fontsize)
-    ax[0][1].set_ylabel('BTE', fontsize=fontsize)
+    ax[0][1].set_xlabel("Number of tasks seen", fontsize=fontsize)
+    ax[0][1].set_ylabel("log Backward TE", fontsize=fontsize)
     ax[0][1].tick_params(labelsize=ticksize)
-    ax[0][1].hlines(1, 1, num_tasks, colors='grey', linestyles='dashed', linewidth=1.5)
+    ax[0][1].hlines(1, 1, num_tasks, colors="grey", linestyles="dashed", linewidth=1.5)
+    ax[0][1].set_yscale("log")
 
     for i in range(num_tasks):
         et = np.asarray(te[i])
         ns = np.arange(i + 1, num_tasks + 1)
-        ax[1][0].plot(ns, et, c='red', linewidth=2.6)
+        ax[1][0].plot(ns, et, c=clr, linewidth=2.6)
 
-    ax[1][0].set_xlabel('Number of tasks seen', fontsize=fontsize)
-    ax[1][0].set_ylabel('Transfer Efficiency', fontsize=fontsize)
-    # ax[1][0].set_xticks(np.arange(1,10))
+    ax[1][0].set_xlabel("Number of tasks seen", fontsize=fontsize)
+    ax[1][0].set_ylabel("log Transfer Efficiency", fontsize=fontsize)
     ax[1][0].tick_params(labelsize=ticksize)
-    ax[1][0].hlines(1, 1, num_tasks, colors='grey', linestyles='dashed', linewidth=1.5)
+    ax[1][0].hlines(1, 1, num_tasks, colors="grey", linestyles="dashed", linewidth=1.5)
+    ax[1][0].set_yscale("log")
 
     for i in range(num_tasks):
         et = 1 - np.asarray(acc[i])
         ns = np.arange(i + 1, num_tasks + 1)
-        ax[1][1].plot(ns, et, c='red', linewidth=2.6)
+        ax[1][1].plot(ns, et, c=clr, linewidth=2.6)
 
-    ax[1][1].set_xlabel('Number of tasks seen', fontsize=fontsize)
-    ax[1][1].set_ylabel('Accuracy', fontsize=fontsize)
+    ax[1][1].set_xlabel("Number of tasks seen", fontsize=fontsize)
+    ax[1][1].set_ylabel("Accuracy", fontsize=fontsize)
     ax[1][1].tick_params(labelsize=ticksize)
+    ax[1][1].set_yscale("log")
 
 
 def plot_paper_figure(bte, fte):
@@ -265,17 +353,21 @@ def plot_paper_figure(bte, fte):
     ticksize = 20
 
     fig, ax = plt.subplots(1, 2, figsize=(16, 8))
-    ax[0].plot(np.arange(1, num_tasks + 1), fte, c='red', marker='.', markersize=14, linewidth=3)
-    ax[0].hlines(1, 1, num_tasks, colors='grey', linestyles='dashed', linewidth=1.5)
+    ax[0].plot(
+        np.arange(1, num_tasks + 1),
+        fte,
+        c="red",
+        marker=".",
+        markersize=14,
+        linewidth=3,
+    )
+    ax[0].hlines(1, 1, num_tasks, colors="grey", linestyles="dashed", linewidth=1.5)
     ax[0].tick_params(labelsize=ticksize)
-    ax[0].set_xlabel('Number of tasks seen', fontsize=fontsize)
-    ax[0].set_ylabel('log Forward TE', fontsize=fontsize)
+    ax[0].set_xlabel("Number of tasks seen", fontsize=fontsize)
+    ax[0].set_ylabel("log Forward TE", fontsize=fontsize)
     ax[0].set_yticks([1, 1.25, 1.5])
 
-    log_lbl = np.round(
-        np.log([1, 1.25, 1.5]),
-        2
-    )
+    log_lbl = np.round(np.log([1, 1.25, 1.5]), 2)
     labels = [item.get_text() for item in ax[0].get_yticklabels()]
 
     for ii, _ in enumerate(labels):
@@ -291,18 +383,15 @@ def plot_paper_figure(bte, fte):
     for i in range(num_tasks):
         et = np.asarray(bte[i])
         ns = np.arange(i + 1, num_tasks + 1)
-        ax[1].plot(ns, et, c='red', linewidth=2.6)
+        ax[1].plot(ns, et, c="red", linewidth=2.6)
 
-    ax[1].set_xlabel('Number of tasks seen', fontsize=fontsize)
-    ax[1].set_ylabel('log Backward TE', fontsize=fontsize)
+    ax[1].set_xlabel("Number of tasks seen", fontsize=fontsize)
+    ax[1].set_ylabel("log Backward TE", fontsize=fontsize)
     ax[1].tick_params(labelsize=ticksize)
-    ax[1].hlines(1, 1, num_tasks, colors='grey', linestyles='dashed', linewidth=1.5)
+    ax[1].hlines(1, 1, num_tasks, colors="grey", linestyles="dashed", linewidth=1.5)
     ax[1].set_yticks([1, 1.35, 1.7])
 
-    log_lbl = np.round(
-        np.log([1, 1.35, 1.7]),
-        2
-    )
+    log_lbl = np.round(np.log([1, 1.35, 1.7]), 2)
     labels = [item.get_text() for item in ax[1].get_yticklabels()]
 
     for ii, _ in enumerate(labels):
@@ -315,4 +404,4 @@ def plot_paper_figure(bte, fte):
     top_side = ax[1].spines["top"]
     top_side.set_visible(False)
 
-    plt.savefig('language.pdf')
+    plt.savefig("language.pdf")
