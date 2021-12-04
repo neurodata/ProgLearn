b69a968fe4460af080ac9a3ad8d67890f13d909d
amyvanee <amyvanee2000@gmail.com>
2021-12-03 22:02:52 -0500

    Add files via upload

--- docs/tutorials/functions/scene_segmentation_rf_tutorial_functions.py
+++ docs/tutorials/functions/scene_segmentation_rf_tutorial_functions.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+# ========================================================#
 # Contributor: Narayani Wagle
 # load_images - loads images and labels from paths and stores in matrix
 # Inputs:
@@ -10,17 +11,42 @@ import numpy as np
 #   X - np - matrix containing images
 #   Y - np - matrix containing labels
 
-def load_images(imgpaths, lblpaths, dim, flatten_imgs = False):
+
+def load_images(imgpaths, lblpaths, dim, flatten_imgs=False):
     if flatten_imgs:
-        X = np.array([cv2.resize(cv2.imread(imgpath), dim, interpolation=cv2.INTER_NEAREST).flatten().transpose() for imgpath in imgpaths])
-        Y = np.array([cv2.resize(cv2.imread(lblpath), dim, interpolation=cv2.INTER_NEAREST).flatten().transpose() for lblpath in lblpaths])
+        X = np.array(
+            [
+                cv2.resize(cv2.imread(imgpath), dim, interpolation=cv2.INTER_NEAREST)
+                .flatten()
+                .transpose()
+                for imgpath in imgpaths
+            ]
+        )
+        Y = np.array(
+            [
+                cv2.resize(cv2.imread(lblpath), dim, interpolation=cv2.INTER_NEAREST)
+                .flatten()
+                .transpose()
+                for lblpath in lblpaths
+            ]
+        )
     else:
-        X = np.array([cv2.resize(cv2.imread(imgpath), dim, interpolation=cv2.INTER_NEAREST) for imgpath in imgpaths])
-        Y = np.array([cv2.resize(cv2.imread(lblpath), dim, interpolation=cv2.INTER_NEAREST) for lblpath in lblpaths])
-    return X,Y
+        X = np.array(
+            [
+                cv2.resize(cv2.imread(imgpath), dim, interpolation=cv2.INTER_NEAREST)
+                for imgpath in imgpaths
+            ]
+        )
+        Y = np.array(
+            [
+                cv2.resize(cv2.imread(lblpath), dim, interpolation=cv2.INTER_NEAREST)
+                for lblpath in lblpaths
+            ]
+        )
+    return X, Y
+
 
 # Contributor: Amy van Ee
 # function that returns dice coefficient as a measure of segmentation accuracy
 def get_dice(true, test):
     return np.size(test[test == true]) * 2.0 / (np.size(true) + np.size(test))
-
