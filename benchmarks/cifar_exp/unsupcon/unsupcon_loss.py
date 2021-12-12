0d49dd9fb40cbf64ae537c96f53c4271ffb029fa
Dante Basile <dbasile1070@gmail.com>
2021-12-12 15:33:45 -0500

    formatted with black

--- benchmarks/cifar_exp/unsupcon/unsupcon_loss.py
+++ benchmarks/cifar_exp/unsupcon/unsupcon_loss.py
@@ -6,34 +6,38 @@ Dante Basile
 from inception_preprocessing import distorted_bounding_box_crop
 import numpy as np
 import pickle
-#from sklearn.metrics import accuracy_score
+
+# from sklearn.metrics import accuracy_score
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.layers import Activation, Dense, Lambda, RandomFlip, Resizing
 from tensorflow.keras.models import Model, Sequential
-#from tensorflow.keras.applications.resnet50 import preprocess_input
+
+# from tensorflow.keras.applications.resnet50 import preprocess_input
+
 
 class DataGenerator(tf.keras.utils.Sequence):
-  def __init__(self, images, batch_size, shuffle=True):
-    super().__init__()
-    self.images = images
-    self.batch_size = batch_size
-    self.shuffle = shuffle
-    key_array = []
-    self.key_array = np.arange(self.images.shape[0], dtype=np.uint32)
-    self.on_epoch_end()
+    def __init__(self, images, batch_size, shuffle=True):
+        super().__init__()
+        self.images = images
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        key_array = []
+        self.key_array = np.arange(self.images.shape[0], dtype=np.uint32)
+        self.on_epoch_end()
 
-  def __len__(self):
-    return len(self.key_array)//self.batch_size
+    def __len__(self):
+        return len(self.key_array) // self.batch_size
 
-  def __getitem__(self, index):
-    keys = self.key_array[index*self.batch_size:(index+1)*self.batch_size]
-    x = np.asarray(self.images[keys], dtype=np.float32)
-    return x
+    def __getitem__(self, index):
+        keys = self.key_array[index * self.batch_size : (index + 1) * self.batch_size]
+        x = np.asarray(self.images[keys], dtype=np.float32)
+        return x
 
-  def on_epoch_end(self):
-    if self.shuffle:
-      self.key_array = np.random.permutation(self.key_array)
+    def on_epoch_end(self):
+        if self.shuffle:
+            self.key_array = np.random.permutation(self.key_array)
+
 
 def build_rn_enc():
     """
@@ -41,12 +45,11 @@ def build_rn_enc():
     Default output layer has been removed
     New output layer is the average pooling layer (2048)
     """
-    base_model = keras.applications.ResNet50(weights=None,
-                                             include_top=True
-                                            )
+    base_model = keras.applications.ResNet50(weights=None, include_top=True)
     f = Model(inputs=base_model.input, outputs=base_model.layers[-2].output)
     f.compile()
     return f
+
 
 def build_proj_head():
     """
@@ -56,10 +59,11 @@ def build_proj_head():
     Output layer (128)
     """
     g = Sequential()
-    g.add(Dense(128, input_dim=2048)) # W_1
-    g.add(Activation('relu')) # \sigma
-    g.add(Dense(128)) # W_2
+    g.add(Dense(128, input_dim=2048))  # W_1
+    g.add(Activation("relu"))  # \sigma
+    g.add(Dense(128))  # W_2
     return g
+
 
 def distorted_bounding_box_crop_wrapper(image):
     """
@@ -67,13 +71,16 @@ def distorted_bounding_box_crop_wrapper(image):
         area: [.08, 1.]
         aspect ratio: [3/4, 4/3]
     """
-    image, bbox = distorted_bounding_box_crop(image, tf.zeros((1, 0, 4)),
-                                       aspect_ratio_range=(3/4, 4/3),
-                                       area_range=(.08, 1.)
-                                      )
+    image, bbox = distorted_bounding_box_crop(
+        image,
+        tf.zeros((1, 0, 4)),
+        aspect_ratio_range=(3 / 4, 4 / 3),
+        area_range=(0.08, 1.0),
+    )
     return image
 
-def color_distortion(image, s=.8):
+
+def color_distortion(image, s=0.8):
     """
     From paper: A Simple Framework for Contrastive Learning of Visual Representations, Chen et al
     """
@@ -83,18 +90,19 @@ def color_distortion(image, s=.8):
     def color_jitter(x):
         # one can also shuffle the order of following augmentations
         # each time they are applied.
-        x = tf.image.random_brightness(x, max_delta=0.8*s)
-        x = tf.image.random_contrast(x, lower=1-0.8*s, upper=1+0.8*s)
-        x = tf.image.random_saturation(x, lower=1-0.8*s, upper=1+0.8*s)
-        x = tf.image.random_hue(x, max_delta=0.2*s)
+        x = tf.image.random_brightness(x, max_delta=0.8 * s)
+        x = tf.image.random_contrast(x, lower=1 - 0.8 * s, upper=1 + 0.8 * s)
+        x = tf.image.random_saturation(x, lower=1 - 0.8 * s, upper=1 + 0.8 * s)
+        x = tf.image.random_hue(x, max_delta=0.2 * s)
         x = tf.clip_by_value(x, 0, 1)
         return x
+
     def color_drop(x):
         x = tf.image.rgb_to_grayscale(x)
         image = tf.tile(x, [1, 1, 3])
         return image
 
-    def random_apply(f, img, p=1.):
+    def random_apply(f, img, p=1.0):
         """
         Added for implementation
         """
@@ -105,34 +113,39 @@ def color_distortion(image, s=.8):
             return img
 
     # randomly apply transformation with probability p.
-    image = random_apply(color_jitter, image, p=.8)
-    image = random_apply(color_drop, image, p=.2)
+    image = random_apply(color_jitter, image, p=0.8)
+    image = random_apply(color_drop, image, p=0.2)
     return image
+
 
 def get_aug_seq(img_h, img_w):
     """
     X: batch of images
     """
-    aug_seq = keras.Sequential([
-        Lambda(distorted_bounding_box_crop_wrapper),
-        Resizing(224, 224),
-        RandomFlip(mode='horizontal'),
-        Lambda(color_distortion), # if preprocess, ensure this can handle negatives
-        #RandomGaussianBlur(),
-        ])
+    aug_seq = keras.Sequential(
+        [
+            Lambda(distorted_bounding_box_crop_wrapper),
+            Resizing(224, 224),
+            RandomFlip(mode="horizontal"),
+            Lambda(color_distortion),  # if preprocess, ensure this can handle negatives
+            # RandomGaussianBlur(),
+        ]
+    )
     return aug_seq
+
 
 def get_x_tilde(x_batch, N, img_h, img_w, rsz_hw=224):
     """
     Sample two augmentation functions t and t_prime to make x_tilde
     """
-    x_t = np.zeros((2*N, rsz_hw, rsz_hw, 3))
+    x_t = np.zeros((2 * N, rsz_hw, rsz_hw, 3))
     for k in range(N):
         t = get_aug_seq(img_h, img_w)
         t_p = get_aug_seq(img_h, img_w)
-        x_t[2*k] = t(x_batch[k])
-        x_t[2*k + 1] = t_p(x_batch[k])
+        x_t[2 * k] = t(x_batch[k])
+        x_t[2 * k + 1] = t_p(x_batch[k])
     return x_t
+
 
 def sim(z_i, z_j):
     """
@@ -140,38 +153,44 @@ def sim(z_i, z_j):
     """
     return tf.tensordot(z_i, z_j, 1) / (tf.norm(z_i) * tf.norm(z_j))
 
+
 def get_pairwise_sim(z, N):
     """
     use the similarity function to construct a matrix of pairwise similarities
     """
     s_l_l = []
-    for i in range(2*N):
+    for i in range(2 * N):
         s_l = []
-        for j in range(2*N):
+        for j in range(2 * N):
             s_l.append(sim(z[i], z[j]))
         s_l_l.append(tf.stack(s_l))
     return tf.stack(s_l_l)
 
-def contrastive_loss(s, i, j, N, tau=1.):
+
+def contrastive_loss(s, i, j, N, tau=1.0):
     """
     Contrastive loss function for a positive pair of examples
     """
     num = tf.math.exp(s[i, j] / tau)
-    den = tf.constant(0.)
-    for k in range(2*N):
+    den = tf.constant(0.0)
+    for k in range(2 * N):
         if k != i:
             den += tf.math.exp(s[i, k] / tau)
     return -1 * tf.math.log(num / den)
 
+
 def model_loss(z, N):
     s = get_pairwise_sim(z, N)
-    L = tf.constant(0.)
+    L = tf.constant(0.0)
     for k in range(N):
-        L += contrastive_loss(s, 2*k, 2*k + 1, N) + contrastive_loss(s, 2*k + 1, 2*k, N)
-    L /= 2*N
+        L += contrastive_loss(s, 2 * k, 2 * k + 1, N) + contrastive_loss(
+            s, 2 * k + 1, 2 * k, N
+        )
+    L /= 2 * N
     return L
 
-def unsupcon_learning(X_train, n_epochs=10, N=16):
+
+def unsupcon_learning(X_train, n_epochs=100, N=16):
     """
     X: training data
     N: batch size [256, 8192]
@@ -183,7 +202,7 @@ def unsupcon_learning(X_train, n_epochs=10, N=16):
     f = build_rn_enc()
     generator = DataGenerator(X_train, N)
     n_batches = len(generator)
-    optimizer = keras.optimizers.SGD(learning_rate=.2, momentum=.4, nesterov=True)
+    optimizer = keras.optimizers.SGD(learning_rate=0.2, momentum=0.4, nesterov=True)
     loss_train = np.zeros(shape=(n_epochs,), dtype=np.float32)
     for epoch in range(n_epochs):
         g = build_proj_head()
@@ -208,24 +227,26 @@ def unsupcon_learning(X_train, n_epochs=10, N=16):
         loss_epoch.append(epoch_loss_avg.result())
     return f, loss_batch, loss_epoch
 
+
 def main():
     """
     N: hardcoded in unsupcon_learning
     tau: hardcoded in contrastive_loss
     """
     (X_train, y_train), (X_test, y_test) = keras.datasets.cifar10.load_data()
-    #X_train = preprocess_input(X_train)
-    #X_test = preprocess_input(X_test)
-    X_train = X_train / 255.
-    #X_test = X_test / 255.
+    # X_train = preprocess_input(X_train)
+    # X_test = preprocess_input(X_test)
+    X_train = X_train / 255.0
+    # X_test = X_test / 255.
     X_train = X_train[:1000]
-    #X_test = X_test[:1000]
+    # X_test = X_test[:1000]
     f, loss_batch, loss_epoch = unsupcon_learning(X_train)
-    f.save("unsupcon_RN50.h5")
-    with open(r"loss_batch.pickle", 'wb') as outfile:
+    f.save("unsupcon_RN50_e100.h5")
+    with open(r"loss_batch_e100.pickle", "wb") as outfile:
         pickle.dump(list(map(tf.get_static_value, loss_batch)), outfile)
-    with open(r"loss_epoch.pickle", 'wb') as outfile:
+    with open(r"loss_epoch_e100.pickle", "wb") as outfile:
         pickle.dump(list(map(tf.get_static_value, loss_epoch)), outfile)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
