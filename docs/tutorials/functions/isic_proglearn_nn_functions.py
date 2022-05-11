3a87c2a7b1cfbf5245784586e877c5f1867f4dea
Kevin Rao <kevinrao2000@gmail.com>
2022-05-11 19:52:06 -0400

    Performed ISIC ProgLearn NN Experiment.

---
+++ docs/tutorials/functions/isic_proglearn_nn_functions.py
@@ -0,0 +1,60 @@
+from tensorflow import keras
+import tensorflow as tf
+
+def dice(seg, gt):
+    """
+    Compute Dice Score between segmentation and groud truth
+    """
+    intersection = tf.reduce_sum(tf.cast(tf.equal(seg, gt), tf.float32))
+    return intersection * 2.0 / tf.cast(tf.size(seg) + tf.size(gt), tf.float32)
+
+def down_block(x, filters, kernel_size=(3, 3), padding="same", strides=1):
+    """
+    Down Block of UNet
+    """
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    p = keras.layers.MaxPool2D((2, 2), (2, 2))(c)
+    return c, p
+
+def up_block(x, skip, filters, kernel_size=(3, 3), padding="same", strides=1):
+    """
+    Up Block of UNet
+    """
+    us = keras.layers.UpSampling2D((2, 2))(x)
+    concat = keras.layers.Concatenate()([us, skip])
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(concat)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    return c
+
+def bottleneck(x, filters, kernel_size=(3, 3), padding="same", strides=1):
+    """
+    Bottleneck of UNet
+    """
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
+    c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+    return c
+
+def UNet():
+    """
+    UNet Neural network
+    """
+    f = [16, 32, 64, 128, 256]
+    inputs = keras.layers.Input((image_size, image_size, 3))
+    
+    p0 = inputs
+    c1, p1 = down_block(p0, f[0]) 
+    c2, p2 = down_block(p1, f[1]) 
+    c3, p3 = down_block(p2, f[2])
+    c4, p4 = down_block(p3, f[3]) 
+    
+    bn = bottleneck(p4, f[4])
+    
+    u1 = up_block(bn, c4, f[3]) #8 -> 16
+    u2 = up_block(u1, c3, f[2]) #16 -> 32
+    u3 = up_block(u2, c2, f[1]) #32 -> 64
+    u4 = up_block(u3, c1, f[0]) #64 -> 128
+    
+    outputs = keras.layers.Conv2D(1, (1, 1), padding="same", activation="sigmoid")(u4)
+    model = keras.models.Model(inputs, outputs)
+    return model
--- proglearn/voters.py
+++ proglearn/voters.py
@@ -1,9 +1,13 @@
 """
 Main Author: Will LeVine 
 Corresponding Email: levinewill@icloud.com
+
+MLKNNClassificationVoter Author: Narayani Wagle
+Corresponding Email: nwagle1@jhu.edu or nhwagle@gmail.com
 """
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from skmultilearn.adapt import MLkNN
 from sklearn.utils.validation import (
     check_X_y,
     check_array,
@@ -289,3 +293,134 @@ class KNNClassificationVoter(BaseClassificationVoter):
             When the model is not fitted.
         """
         return self.classes[np.argmax(self.predict_proba(X), axis=1)]
+
+
+class MLKNNClassificationVoter(BaseClassificationVoter):
+    """
+    A class used to vote on data under any transformer outputting data
+    in continuous Euclidean space, which inherits from the BaseClassificationVoter
+    class in base.py.
+
+    Parameters
+    ----------
+    k : int
+        integer indicating number of neighbors to use for each prediction during
+        fitting and voting
+
+    kwargs : dictionary, default={}
+        contains all keyword arguments for the underlying KNN
+
+    classes : list, default=[]
+        list of all possible output label values
+
+    Attributes
+    ----------
+    missing_label_indices_ : list
+        a (potentially empty) list of label values
+        that exist in the ``classes`` parameter but
+        are missing in the latest ``fit`` function
+        call
+
+    knn_ : sklearn.neighbors.KNeighborsClassifier
+        the internal sklearn instance of KNN
+        classifier
+    """
+
+    def __init__(self, k=None, kwargs={}, classes=[]):
+        self.k = k
+        self.kwargs = kwargs
+        self.classes = np.asarray(classes)
+
+    def fit(self, X, y):
+        """
+        Fits data X given class labels y.
+
+        Parameters
+        ----------
+        X : array of shape [n_samples, n_features]
+            the transformed data that will be trained on
+        y : array of shape [n_samples, n_features]
+            the label for class membership of the given data
+
+        Returns
+        -------
+        self : MLKNNClassificationVoter
+            The object itself.
+        """
+        y[y > 0] = 1
+        y[y <= 0] = 0
+        # X, y = check_X_y(X, y, multi_output=True)
+        k = int(np.log2(len(X))) if self.k == None else self.k
+        self.mlknn_ = MLkNN(k, **self.kwargs)
+
+        # flatten X and y such that each sample is one dimensional
+        X_flattened = X.reshape(X.shape[0], -1)
+        y_flattened = y.reshape(y.shape[0], -1)
+
+        self.mlknn_.fit(X_flattened, y_flattened)
+
+        # missing label indices should not be issue in scene segmentation
+        num_classes = len(np.unique(y))
+        self.missing_label_indices_ = []
+
+        if self.classes.size != 0 and num_classes < len(self.classes):
+            for idx, label in enumerate(self.classes):
+                if label not in np.unique(y):
+                    self.missing_label_indices_.append(idx)
+
+        return self
+
+    def predict_proba(self, X):
+        """
+        Returns the posterior probabilities of each class for data X.
+
+        Parameters
+        ----------
+        X : array of shape [n_samples, n_features]
+            the transformed input data
+
+        Returns
+        -------
+        y_proba_hat : ndarray of shape [n_samples, n_classes]
+            posteriors per example
+
+        Raises
+        ------
+        NotFittedError
+            When the model is not fitted.
+        """
+        check_is_fitted(self)
+        # X = check_array(X)
+
+        # flatten X such that each sample is one dimensional
+        X_flattened = X.reshape(X.shape[0], -1)
+
+        votes_per_example = self.mlknn_.predict_proba(X_flattened)
+
+        if len(self.missing_label_indices_) > 0:
+            for i in self.missing_label_indices_:
+                new_col = np.zeros(votes_per_example.shape[0])
+                votes_per_example = np.insert(votes_per_example, i, new_col, axis=1)
+
+        return votes_per_example
+
+    def predict(self, X):
+        """
+        Returns the predicted class labels for data X.
+
+        Parameters
+        ----------
+        X : array of shape [n_samples, n_features]
+            the transformed input data
+
+        Returns
+        -------
+        y_hat : ndarray of shape [n_samples, n_features]
+            predicted class label per example
+
+        Raises
+        ------
+        NotFittedError
+            When the model is not fitted.
+        """
+        return (self.predict_proba(X) > 0.5).astype(int)
