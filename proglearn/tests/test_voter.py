a3e9c4a5c97c757e914963420ece67f42f90eb6a
Kevin Rao <kevinrao2000@gmail.com>
2022-05-17 01:38:52 -0400

    Fix attempt for tests.

--- proglearn/tests/test_voter.py
+++ proglearn/tests/test_voter.py
@@ -83,17 +83,21 @@ class TestKNNClassificationVoter:
             np.random.seed(0)
 
             # generate training data and classes
-            X = np.concatenate((np.zeros(100, 10), np.ones(100, 10))).reshape(-1, 10)
-            Y = np.concatenate((np.zeros(100, 10), np.ones(100, 10))).reshape(-1, 10)
+            X = np.concatenate((np.zeros((100, 10)), np.ones((100, 10)))).reshape(
+                -1, 10
+            )
+            Y = np.concatenate((np.zeros((100, 10)), np.ones((100, 10)))).reshape(
+                -1, 10
+            )
 
             # train model
             mlkcv = MLKNNClassificationVoter(3)
             mlkcv.fit(X, Y)
 
             # generate testing data and class probability
-            X_test = np.ones(6, 10).reshape(-1, 10)
+            X_test = np.ones((6, 10)).reshape(-1, 10)
 
-            Y_test = np.ones(6, 10).reshape(-1, 10)
+            Y_test = np.ones((6, 10)).reshape(-1, 10)
 
             # check if model predicts as expected
             testing.assert_allclose(Y_test, mlkcv.predict_proba(X_test), atol=1e-4)
